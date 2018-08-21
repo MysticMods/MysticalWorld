cc32597d77894afab459f3a8107ee0fe8f53374d
EpicSquid <epicsquid315@gmail.com>
2018-08-21 20:44:26 +1000

    Added Pereskia, Terra Moss, Spirit Herb and Wildroot crops

--- src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
+++ src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
@@ -6,19 +6,27 @@ import epicsquid.mysticallib.block.BlockCropBase;
 import epicsquid.mysticallib.event.RegisterContentEvent;
 import epicsquid.mysticalworld.item.BlockAubergineCrop;
 import epicsquid.mysticalworld.item.BlockMoonglowCrop;
+import epicsquid.mysticalworld.item.BlockPereskiaCrop;
+import epicsquid.mysticalworld.item.BlockSpiritHerbCrop;
+import epicsquid.mysticalworld.item.BlockTerraMossCrop;
+import epicsquid.mysticalworld.item.BlockWildrootCrop;
 import net.minecraft.block.Block;
 import net.minecraftforge.common.EnumPlantType;
 
 public class ModBlocks {
 
   // All blocks
-  public static Block moonglow, aubergine;
+  public static Block moonglow, aubergine, pereskia, terra_moss, wildroot, spirit_herb;
 
   /**
    * Register all blocks
    */
   public static void registerBlocks(@Nonnull RegisterContentEvent event) {
-    event.addBlock(moonglow = new BlockMoonglowCrop("moonglow", EnumPlantType.Crop));
-    event.addBlock(aubergine = new BlockAubergineCrop("aubergine", EnumPlantType.Crop));
+    event.addBlock(moonglow = new BlockMoonglowCrop("moonglow_crop", EnumPlantType.Crop));
+    event.addBlock(aubergine = new BlockAubergineCrop("aubergine_crop", EnumPlantType.Crop));
+    event.addBlock(pereskia = new BlockPereskiaCrop("pereskia_crop", EnumPlantType.Crop));
+    event.addBlock(terra_moss = new BlockTerraMossCrop("terra_moss_crop", EnumPlantType.Crop));
+    event.addBlock(wildroot = new BlockWildrootCrop("wildroot_crop", EnumPlantType.Crop));
+    event.addBlock(spirit_herb = new BlockSpiritHerbCrop("spirit_herb_crop", EnumPlantType.Crop));
   }
 }
--- src/main/java/epicsquid/mysticalworld/init/ModItems.java
+++ src/main/java/epicsquid/mysticalworld/init/ModItems.java
@@ -12,7 +12,8 @@ import net.minecraft.item.Item;
 public class ModItems {
 
   // All mod items
-  public static Item carapace, pelt, moonglow_seed, moonglow_leaf, aubergine, aubergine_seed;
+  public static Item carapace, pelt, moonglow_seed, moonglow_leaf, aubergine, aubergine_seed, pereskia_bulb, pereskia, terra_moss_seed, terra_moss, spirit_herb,
+      spirit_herb_seed, wildroot;
 
   /**
    * Register all items
@@ -24,5 +25,12 @@ public class ModItems {
     event.addItem(moonglow_leaf = new ItemBase("moonglow_leaf").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
     event.addItem(aubergine_seed = new ItemSeedBase("aubergine_seed", ModBlocks.aubergine, Blocks.DIRT).setModelCustom(true).setCreativeTab(MysticalWorld.tab));
     event.addItem(aubergine = new ItemBase("aubergine").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+    event.addItem(pereskia_bulb = new ItemSeedBase("pereskia_bulb", ModBlocks.pereskia, Blocks.DIRT).setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+    event.addItem(pereskia = new ItemBase("pereskia").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+    event.addItem(terra_moss_seed = new ItemSeedBase("terra_moss_seed", ModBlocks.terra_moss, Blocks.DIRT).setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+    event.addItem(terra_moss = new ItemBase("terra_moss").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+    event.addItem(spirit_herb_seed = new ItemSeedBase("spirit_herb_seed", ModBlocks.spirit_herb, Blocks.DIRT).setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+    event.addItem(spirit_herb = new ItemBase("spirit_herb").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+    event.addItem(wildroot = new ItemSeedBase("wildroot", ModBlocks.wildroot, Blocks.DIRT).setModelCustom(true).setCreativeTab(MysticalWorld.tab));
   }
 }
