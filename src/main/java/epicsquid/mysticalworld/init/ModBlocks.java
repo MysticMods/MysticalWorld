816e80c90329d7e419f9f0a959c216665a32c7ba
Unknown <mcpkmart@hotmail.nl>
2018-10-06 10:08:57 +0200

    Added Thatch

--- src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
+++ src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
@@ -3,19 +3,23 @@ package epicsquid.mysticalworld.init;
 import javax.annotation.Nonnull;
 
 import epicsquid.mysticallib.event.RegisterContentEvent;
+import epicsquid.mysticalworld.MysticalWorld;
 import epicsquid.mysticalworld.block.BlockAubergineCrop;
 import epicsquid.mysticalworld.block.BlockMoonglowCrop;
 import epicsquid.mysticalworld.block.BlockPereskiaCrop;
 import epicsquid.mysticalworld.block.BlockSpiritHerbCrop;
 import epicsquid.mysticalworld.block.BlockTerraMossCrop;
+import epicsquid.mysticalworld.block.BlockThatch;
 import epicsquid.mysticalworld.block.BlockWildrootCrop;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 import net.minecraftforge.common.EnumPlantType;
 
 public class ModBlocks {
 
   // All blocks
-  public static Block moonglow, aubergine, pereskia, terra_moss, wildroot, spirit_herb;
+  public static Block moonglow, aubergine, pereskia, terra_moss, wildroot, spirit_herb, thatch;
 
   /**
    * Register all blocks
@@ -27,5 +31,6 @@ public class ModBlocks {
     event.addBlock(terra_moss = new BlockTerraMossCrop("terra_moss_crop", EnumPlantType.Crop));
     event.addBlock(wildroot = new BlockWildrootCrop("wildroot_crop", EnumPlantType.Crop));
     event.addBlock(spirit_herb = new BlockSpiritHerbCrop("spirit_herb_crop", EnumPlantType.Crop));
+    event.addBlock(thatch = new BlockThatch(Material.LEAVES, SoundType.PLANT, 0.8f, "thatch")).setCreativeTab(MysticalWorld.tab);
   }
 }
