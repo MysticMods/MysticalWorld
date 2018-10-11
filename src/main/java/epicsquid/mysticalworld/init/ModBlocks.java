b36f5d8f54c96242bbc87a3b949f4eacc8b4cdba
EpicSquid <epicsquid315@gmail.com>
2018-10-11 14:27:11 +1100

    Added Caminite blocks

--- src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
+++ src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
@@ -2,6 +2,10 @@ package epicsquid.mysticalworld.init;
 
 import javax.annotation.Nonnull;
 
+import epicsquid.mysticallib.block.BlockBase;
+import epicsquid.mysticallib.block.BlockSlabBase;
+import epicsquid.mysticallib.block.BlockStairsBase;
+import epicsquid.mysticallib.block.BlockWallBase;
 import epicsquid.mysticallib.event.RegisterContentEvent;
 import epicsquid.mysticalworld.MysticalWorld;
 import epicsquid.mysticalworld.block.BlockAubergineCrop;
@@ -19,12 +23,17 @@ import net.minecraftforge.common.EnumPlantType;
 public class ModBlocks {
 
   // All blocks
-  public static Block moonglow, aubergine, pereskia, terra_moss, wildroot, spirit_herb, thatch;
+  public static Block moonglow, aubergine, pereskia, terra_moss, wildroot, spirit_herb, thatch, caminite, caminite_stairs, caminite_slab, caminite_wall,
+      caminite_bricks, caminite_bricks_stairs, caminite_bricks_slab, caminite_bricks_wall;
 
   /**
    * Register all blocks
    */
   public static void registerBlocks(@Nonnull RegisterContentEvent event) {
+
+    BlockSlabBase slab_temp, double_slab_temp;
+
+    // Roots
     event.addBlock(moonglow = new BlockMoonglowCrop("moonglow_crop", EnumPlantType.Crop));
     event.addBlock(aubergine = new BlockAubergineCrop("aubergine_crop", EnumPlantType.Crop));
     event.addBlock(pereskia = new BlockPereskiaCrop("pereskia_crop", EnumPlantType.Crop));
@@ -32,5 +41,32 @@ public class ModBlocks {
     event.addBlock(wildroot = new BlockWildrootCrop("wildroot_crop", EnumPlantType.Crop));
     event.addBlock(spirit_herb = new BlockSpiritHerbCrop("spirit_herb_crop", EnumPlantType.Crop));
     event.addBlock(thatch = new BlockThatch(Material.LEAVES, SoundType.PLANT, 0.8f, "thatch")).setCreativeTab(MysticalWorld.tab);
+
+    // Embers
+    event.addBlock(caminite = new BlockBase(Material.ROCK, SoundType.STONE, 2.0f, "caminite").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+    event.addBlock(caminite_stairs = new BlockStairsBase(caminite.getDefaultState(), SoundType.STONE, 2.0f, "caminite_stairs").setModelCustom(true)
+        .setCreativeTab(MysticalWorld.tab));
+    double_slab_temp = new BlockSlabBase(Material.ROCK, SoundType.STONE, 2.0f, "caminite_double_slab", caminite.getDefaultState(), true, null)
+        .setModelCustom(true);
+    slab_temp = new BlockSlabBase(Material.ROCK, SoundType.STONE, 2.0f, "caminite_slab", caminite.getDefaultState(), false, double_slab_temp)
+        .setModelCustom(true);
+    double_slab_temp.setSlab(slab_temp);
+    event.addBlock(caminite_slab = slab_temp.setCreativeTab(MysticalWorld.tab));
+    event.addBlock(double_slab_temp);
+    event.addBlock(caminite_wall = new BlockWallBase(caminite, SoundType.STONE, 2.0f, "caminite_wall").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+
+    event.addBlock(
+        caminite_bricks = new BlockBase(Material.ROCK, SoundType.STONE, 2.0f, "caminite_bricks").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+    event.addBlock(caminite_bricks_stairs = new BlockStairsBase(caminite_bricks.getDefaultState(), SoundType.STONE, 2.0f, "caminite_bricks_stairs").setModelCustom(true)
+        .setCreativeTab(MysticalWorld.tab));
+    double_slab_temp = new BlockSlabBase(Material.ROCK, SoundType.STONE, 2.0f, "caminite_bricks_double_slab", caminite_bricks.getDefaultState(), true, null)
+        .setModelCustom(true);
+    slab_temp = new BlockSlabBase(Material.ROCK, SoundType.STONE, 2.0f, "caminite_bricks_slab", caminite_bricks.getDefaultState(), false, double_slab_temp)
+        .setModelCustom(true);
+    double_slab_temp.setSlab(slab_temp);
+    event.addBlock(caminite_bricks_slab = slab_temp.setCreativeTab(MysticalWorld.tab));
+    event.addBlock(double_slab_temp);
+    event.addBlock(caminite_bricks_wall = new BlockWallBase(caminite_bricks, SoundType.STONE, 2.0f, "caminite_bricks_wall").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+
   }
 }
