8f0c95fa39b916e4ed3aa2c38af3f792e9418863
EpicSquid <epicsquid315@gmail.com>
2019-01-21 18:10:02 +1100

    Added wildewheet

--- src/main/java/epicsquid/mysticalworld/init/ModItems.java
+++ src/main/java/epicsquid/mysticalworld/init/ModItems.java
@@ -20,7 +20,7 @@ import sun.security.krb5.Config;
 public class ModItems {
 
   // All mod items
-  public static Item iron_dust, iron_dust_tiny, gold_dust, gold_dust_tiny, carapace, pelt, moonglow_seed, moonglow_leaf, aubergine, aubergine_seed, pereskia_bulb, pereskia, terra_spores, terra_moss, spirit_herb,
+  public static Item iron_dust, iron_dust_tiny, gold_dust, gold_dust_tiny, carapace, pelt, moonglow_seed, moonglow_leaf, aubergine, aubergine_seed, pereskia_bulb, pereskia, terra_spores, terra_moss, spirit_herb, wildewheet,
       spirit_herb_seed, wildroot, fungus_cap, teapot, bark_oak, bark_birch, bark_spruce, bark_jungle, bark_dark_oak, bark_acacia, gold_knife, diamond_knife, iron_knife, stone_knife, wood_knife;
 
   /**
@@ -54,6 +54,7 @@ public class ModItems {
       event.addItem(spirit_herb = new ItemBase("spirit_herb").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
       event.addItem(wildroot = new ItemSeedBase("wildroot", ModBlocks.wildroot, Blocks.DIRT).setModelCustom(true).setCreativeTab(MysticalWorld.tab));
       event.addItem(fungus_cap = new ItemBase("fungus_cap").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
+      event.addItem(wildewheet = new ItemBase("widlewheet").setModelCustom(true).setCreativeTab(MysticalWorld.tab));
 
       registerSeedDrops();
 
