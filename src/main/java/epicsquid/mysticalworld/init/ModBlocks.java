8fad393d9ac2a87d4fe82e72a75d01f1d14647cf
Mart <mcpkmart@hotmail.nl>
2019-03-04 16:49:06 +0100

    Moved solar blocks to Solar

--- src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
+++ src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
@@ -17,7 +17,7 @@ import net.minecraft.block.material.Material;
 public class ModBlocks {
 
   // All blocks
-  public static Block solar_infused_stone, solar_infused_stone_stairs, solar_infused_stone_slab, solar_infused_stone_wall;
+
 
 
 
@@ -26,7 +26,7 @@ public class ModBlocks {
    */
   public static void registerBlocks(@Nonnull RegisterContentEvent event) {
 
-    BlockSlabBase slab_temp, double_slab_temp;
+
 
     if (ConfigManager.modules.rootsModuleEnabled) {
 
@@ -37,20 +37,7 @@ public class ModBlocks {
     }
 
     if (ConfigManager.modules.solarModuleEnabled) {
-      //Solar
-      event.addBlock(solar_infused_stone = new BlockBase(Material.ROCK, SoundType.STONE, 1.4f, "solar_infused_stone").setCreativeTab(MysticalWorld.tab));
-      event.addBlock(
-          solar_infused_stone_stairs = new BlockStairsBase(solar_infused_stone.getDefaultState(), SoundType.STONE, 2.0f, "solar_infused_stone_stairs").setModelCustom(true)
-              .setCreativeTab(MysticalWorld.tab));
-      double_slab_temp = new BlockSlabBase(Material.ROCK, SoundType.STONE, 2.0f, "solar_infused_stone_double_slab", solar_infused_stone.getDefaultState(), true, null)
-          .setModelCustom(true);
-      slab_temp = new BlockSlabBase(Material.ROCK, SoundType.STONE, 2.0f, "solar_infused_stone_slab", solar_infused_stone.getDefaultState(), false, double_slab_temp)
-          .setModelCustom(true);
-      double_slab_temp.setSlab(slab_temp);
-      event.addBlock(solar_infused_stone_slab = slab_temp.setCreativeTab(MysticalWorld.tab));
-      event.addBlock(double_slab_temp);
-      event.addBlock(solar_infused_stone_wall = new BlockWallBase(solar_infused_stone, SoundType.STONE, 2.0f, "solar_infused_stone_wall").setModelCustom(true)
-          .setCreativeTab(MysticalWorld.tab));
+
     }
   }
 
