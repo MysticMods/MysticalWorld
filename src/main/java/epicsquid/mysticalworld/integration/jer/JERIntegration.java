0ee87ce5fad8d578e3f57859ca10513beefa7e56
Jon McManus <due@wxwhatever.com>
2019-04-11 19:56:28 +1000

    Remove some unused imports.

--- src/main/java/epicsquid/mysticalworld/integration/jer/JERIntegration.java
+++ src/main/java/epicsquid/mysticalworld/integration/jer/JERIntegration.java
@@ -9,15 +9,9 @@ import jeresources.api.IJERAPI;
 import jeresources.api.IMobRegistry;
 import jeresources.api.JERPlugin;
 import jeresources.api.conditionals.LightLevel;
-import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class JERIntegration {
