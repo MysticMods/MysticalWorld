9ed17a174f14489ba4df7c002d3be936790363b0
Jon McManus <due@wxwhatever.com>
2019-05-03 20:35:49 +1000

    Tameable, sitting foxes!
    Not yet sleeping. Conditions for sleeping will be: during the day, after
    eating, after mating, or if a certain period of time has elapsed since
    they last slept.

--- src/main/java/epicsquid/mysticalworld/entity/model/ModelFox.java
+++ src/main/java/epicsquid/mysticalworld/entity/model/ModelFox.java
@@ -1,10 +1,12 @@
 package epicsquid.mysticalworld.entity.model;
 
-import javax.annotation.Nonnull;
-
+import epicsquid.mysticalworld.entity.EntityFox;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+
+import javax.annotation.Nonnull;
 
 /**
  * ModelFox - Elucent
@@ -27,6 +29,7 @@ public class ModelFox extends ModelBase {
   private ModelRenderer snout;
   private ModelRenderer earR;
   private ModelRenderer earL;
+  private int state = 0;
 
   public ModelFox() {
     this.textureWidth = 32;
@@ -109,22 +112,93 @@ public class ModelFox extends ModelBase {
 
   @Override
   public void render(@Nonnull Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor) {
+    this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entity);
+    this.body1.render(scaleFactor);
+  }
+
+  @Override
+  public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn) {
     float sin = (float) Math.sin(ageInTicks * 0.125f * (Math.PI * 2.0f));
     float cos = (float) Math.cos(ageInTicks * 0.0625f * (Math.PI * 2.0f));
-    this.earL.rotateAngleZ = 0.5235987755982988F + getBobble(60, ageInTicks) * 0.0981747703F;
-    this.earR.rotateAngleZ = -0.5235987755982988F - getBobble(130, ageInTicks) * 0.0981747703F;
-    this.backL.rotateAngleX = limbSwingAmount * sin;
-    this.frontR.rotateAngleX = limbSwingAmount * sin;
-    this.backR.rotateAngleX = -limbSwingAmount * sin;
-    this.frontL.rotateAngleX = -limbSwingAmount * sin;
-    this.head.rotateAngleX = headPitch * 0.017453292F;
-    this.head.rotateAngleY = netHeadYaw * 0.017453292F;
-    this.tail1.rotateAngleX = 1.1780972450961724F + limbSwingAmount;
-    this.tail1.rotateAngleZ = limbSwingAmount * 0.375f * cos + 0.0872664626F * getBobble(45, ageInTicks);
-    this.tail2.rotateAngleZ = limbSwingAmount * 0.375f * cos + 0.0872664626F * getBobble(90, ageInTicks);
-    this.tail3.rotateAngleZ = limbSwingAmount * 0.375f * cos + 0.0872664626F * getBobble(135, ageInTicks);
-    this.tail4.rotateAngleZ = limbSwingAmount * 0.375f * cos + 0.0872664626F * getBobble(180, ageInTicks);
-    this.body1.render(scaleFactor);
+    if (state == 0) {
+      this.earL.rotateAngleZ = 0.5235987755982988F + getBobble(60, ageInTicks) * 0.0981747703F;
+      this.earR.rotateAngleZ = -0.5235987755982988F - getBobble(130, ageInTicks) * 0.0981747703F;
+      this.backL.rotateAngleX = limbSwingAmount * sin;
+      this.frontR.rotateAngleX = limbSwingAmount * sin;
+      this.backR.rotateAngleX = -limbSwingAmount * sin;
+      this.frontL.rotateAngleX = -limbSwingAmount * sin;
+      this.head.rotateAngleX = headPitch * 0.017453292F;
+      this.head.rotateAngleY = netHeadYaw * 0.017453292F;
+      this.tail1.rotateAngleX = 1.1780972450961724F + limbSwingAmount;
+      this.tail1.rotateAngleZ = limbSwingAmount * 0.375f * cos + 0.0872664626F * getBobble(45, ageInTicks);
+      this.tail2.rotateAngleZ = limbSwingAmount * 0.375f * cos + 0.0872664626F * getBobble(90, ageInTicks);
+      this.tail3.rotateAngleZ = limbSwingAmount * 0.375f * cos + 0.0872664626F * getBobble(135, ageInTicks);
+      this.tail4.rotateAngleZ = limbSwingAmount * 0.375f * cos + 0.0872664626F * getBobble(180, ageInTicks);
+      this.setRotateAngle(body1, 0F, 0.0F, 0.0F);
+      this.setRotateAngle(body2, 0F, 0.0F, 0.0F);
+    } else if (state == 1) { // sitting
+      this.setRotateAngle(backL, -0.8196066167365371F, -0.31869712141416456F, 0.0F);
+      this.setRotateAngle(backR, -0.8196066167365371F, 0.36425021489121656F, 0.0F);
+      this.setRotateAngle(body1, -0.6829473363053812F, 0.0F, 0.0F);
+      this.setRotateAngle(body2, -0.045553093477052F, 0.0F, 0.0F);
+      this.setRotateAngle(earL, 0.0F, -0.2617993877991494F, 0.6085963101704227F);
+      this.setRotateAngle(earR, 0.0F, 0.2617993877991494F, -0.5988224663592545F);
+      this.setRotateAngle(frontL, 0.31869712141416456F, 0.0F, 0.0F);
+      this.setRotateAngle(frontR, 0.31869712141416456F, 0.0F, 0.0F);
+      this.setRotateAngle(neck, 0.36425021489121656F, 0.0F, 0.0F);
+      this.setRotateAngle(snout, 0.17453292519943295F, 0.0F, 0.0F);
+      this.setRotateAngle(tail1, 2.5497515042385164F, 0.0F, 0.06178465552059926F);
+      this.setRotateAngle(tail2, -0.39269908169872414F, 0.0F, 0.08726646259971647F);
+      this.setRotateAngle(tail3, 0.0F, 0.0F, 0.06178465552059926F);
+      this.setRotateAngle(tail4, 0.39269908169872414F, 0.0F, 0.0F);
+    } else if (state == 2) { // sleeping
+      this.setRotateAngle(earL, 0.5462880558742251F, 0.0F, 0.7740535232594852F);
+      this.setRotateAngle(frontR, -0.6373942428283291F, 0.12636183784438945F, -1.5025539530419183F);
+      this.setRotateAngle(earR, 0.40980330836826856F, 0.2617993877991494F, -0.9560913642424937F);
+      this.setRotateAngle(backR, -1.5025539530419183F, -0.091106186954104F, 0.0F);
+      this.setRotateAngle(backL, -1.5025539530419183F, -0.31869712141416456F, -0.045553093477052F);
+      this.setRotateAngle(tail1, 0.8196066167365371F, 0.091106186954104F, -1.3658946726107624F);
+      this.setRotateAngle(frontL, 0.091106186954104F, 0.0F, -1.4570008595648662F);
+      this.setRotateAngle(body2, -0.091106186954104F, 0.18203784098300857F, 0.0F);
+      this.setRotateAngle(tail3, -0.8651597102135892F, 0.0F, -0.18203784098300857F);
+      this.setRotateAngle(neck, 0.0F, -1.0016444577195458F, 0.091106186954104F);
+      this.setRotateAngle(tail4, -0.36425021489121656F, 0.0F, 0.0F);
+      this.setRotateAngle(body1, 0.017453292519943295F, -0.36425021489121656F, 0.0F);
+      this.setRotateAngle(tail2, -0.9208357133522083F, 0.091106186954104F, 0.091106186954104F);
+      this.setRotateAngle(head, 0.045553093477052F, -0.18203784098300857F, 0.0F);
+      this.setRotateAngle(snout, -0.091106186954104F, 0.0F, 0.0F);
+    }
+  }
+
+  @Override
+  public void setLivingAnimations(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTickTime) {
+    EntityFox fox = (EntityFox) entitylivingbaseIn;
+
+    this.backL.setRotationPoint(1.0F, 4.0F, 1.5F);
+    this.backR.setRotationPoint(-1.0F, 4.0F, 1.5F);
+    this.body1.setRotationPoint(0.0F, 15.5F, -2.0F);
+    this.body2.setRotationPoint(0.0F, 0.0F, 5.0F);
+    this.earL.setRotationPoint(1.5F, -1.0F, -1.0F);
+    this.earR.setRotationPoint(-1.5F, -1.0F, -1.0F);
+    this.frontL.setRotationPoint(1.0F, 4.0F, 1.0F);
+    this.frontR.setRotationPoint(-1.0F, 4.0F, 1.0F);
+    this.head.setRotationPoint(0.0F, 0.0F, -4.0F);
+    this.neck.setRotationPoint(0.0F, 1.5F, 2.0F);
+    this.snout.setRotationPoint(0.0F, 0.0F, -3.0F);
+    this.tail1.setRotationPoint(0.0F, 1.0F, 2.5F);
+    this.tail2.setRotationPoint(0.0F, 1.5F, 0.0F);
+    this.tail3.setRotationPoint(0.0F, 3.0F, 0.0F);
+    this.tail4.setRotationPoint(0.0F, 3.5F, 0.0F);
+
+    if (fox.isSitting()) {
+      state = 1;
+    } else if (fox.isSleeping()) {
+      state = 2;
+    } else {
+      state = 0;
+    }
+
+    super.setLivingAnimations(entitylivingbaseIn, limbSwing, limbSwingAmount, partialTickTime);
   }
 
   /**
