9f8f1e44b23eb8759f6d3c7453ff95ede3f6fc0c
Unknown <roxxanrebirth@gmail.com>
2019-06-06 00:14:01 -0400

    Fixed in Squid's Format

--- src/main/java/epicsquid/mysticalworld/events/SquidHandler.java
+++ src/main/java/epicsquid/mysticalworld/events/SquidHandler.java
@@ -20,34 +20,34 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber(modid = MysticalWorld.MODID)
 public class SquidHandler {
 
-    @SubscribeEvent
-    public static void attackCapability(AttachCapabilitiesEvent<Entity> event){
-        if(event.getObject()instanceof EntitySquid){
-            event.addCapability(AnimalCooldownCapabilityProvider.IDENTIFIER, new AnimalCooldownCapabilityProvider());
-        }
+  @SubscribeEvent
+  public static void attackCapability(AttachCapabilitiesEvent<Entity> event) {
+    if (event.getObject() instanceof EntitySquid) {
+      event.addCapability(AnimalCooldownCapabilityProvider.IDENTIFIER, new AnimalCooldownCapabilityProvider());
     }
+  }
 
-    @SubscribeEvent
-    public static void onSquidMilked(PlayerInteractEvent.EntityInteract event){
-        if(!event.getWorld().isRemote){
-            EntityPlayer player = (EntityPlayer) event.getEntity();
-            ItemStack heldItem = player.getHeldItem(event.getHand());
-            if(!heldItem.isEmpty() && heldItem.getItem() instanceof ItemGlassBottle){
-                if(event.getTarget() instanceof EntitySquid){
-                    AnimalCooldownCapability cap = event.getTarget().getCapability(AnimalCooldownCapabilityProvider.ANIMAL_COOLDOWN_CAPABILITY,null);
-                    if(cap != null){
-                        if(cap.canHarvest()){
-                            cap.setCooldown(20*15);
-                            event.getWorld().playSound(player.posX, player.posY, player.posZ, SoundEvents.ENTITY_COW_MILK, SoundCategory.PLAYERS, 0.5F, event.getWorld().rand.nextFloat() * 0.25F + 0.6F, true);
-                            if(!player.isCreative()) heldItem.shrink(1);
-                            player.inventory.addItemStackToInventory(new ItemStack(ModItems.ink_bottle));
-                            event.setCanceled(true);
-                            event.setCancellationResult(EnumActionResult.SUCCESS);
-                        }
-                    }
-
-                }
+  @SubscribeEvent
+  public static void onSquidMilked(PlayerInteractEvent.EntityInteract event) {
+    if (!event.getWorld().isRemote) {
+      EntityPlayer player = (EntityPlayer) event.getEntity();
+      ItemStack heldItem = player.getHeldItem(event.getHand());
+      if (!heldItem.isEmpty() && heldItem.getItem() instanceof ItemGlassBottle) {
+        if (event.getTarget() instanceof EntitySquid) {
+          AnimalCooldownCapability cap = event.getTarget().getCapability(AnimalCooldownCapabilityProvider.ANIMAL_COOLDOWN_CAPABILITY, null);
+          if (cap != null) {
+            if (cap.canHarvest()) {
+              cap.setCooldown(20 * 15);
+              event.getWorld().playSound(player.posX, player.posY, player.posZ, SoundEvents.ENTITY_COW_MILK, SoundCategory.PLAYERS, 0.5F, event.getWorld().rand.nextFloat() * 0.25F + 0.6F, true);
+              if (!player.isCreative()) heldItem.shrink(1);
+              player.inventory.addItemStackToInventory(new ItemStack(ModItems.ink_bottle));
+              event.setCanceled(true);
+              event.setCancellationResult(EnumActionResult.SUCCESS);
             }
+          }
+
         }
+      }
     }
+  }
 }
