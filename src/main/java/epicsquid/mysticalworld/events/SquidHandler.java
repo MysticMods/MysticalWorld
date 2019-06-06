fbd38153bd642b82d644d8d86b6bd91e292d3ab9
Jon McManus <due@wxwhatever.com>
2019-06-06 22:35:43 +1000

    Give squid's a "not enough ink" message.

--- src/main/java/epicsquid/mysticalworld/events/SquidHandler.java
+++ src/main/java/epicsquid/mysticalworld/events/SquidHandler.java
@@ -1,7 +1,7 @@
 package epicsquid.mysticalworld.events;
 
-import epicsquid.mysticalworld.MysticalWorld;
 import epicsquid.mysticallib.capability.EntityCooldownCapability;
+import epicsquid.mysticalworld.MysticalWorld;
 import epicsquid.mysticalworld.capability.AnimalCooldownCapabilityProvider;
 import epicsquid.mysticalworld.init.ModItems;
 import net.minecraft.entity.Entity;
@@ -12,6 +12,9 @@ import net.minecraft.item.ItemGlassBottle;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -29,11 +32,13 @@ public class SquidHandler {
 
   @SubscribeEvent
   public static void onSquidMilked(PlayerInteractEvent.EntityInteract event) {
-    if (!event.getWorld().isRemote) {
-      EntityPlayer player = (EntityPlayer) event.getEntity();
-      ItemStack heldItem = player.getHeldItem(event.getHand());
-      if (!heldItem.isEmpty() && heldItem.getItem() instanceof ItemGlassBottle) {
-        if (event.getTarget() instanceof EntitySquid) {
+    EntityPlayer player = (EntityPlayer) event.getEntity();
+    ItemStack heldItem = player.getHeldItem(event.getHand());
+    if (!heldItem.isEmpty() && heldItem.getItem() instanceof ItemGlassBottle) {
+      if (event.getTarget() instanceof EntitySquid) {
+        event.setCanceled(true);
+        event.setCancellationResult(EnumActionResult.SUCCESS);
+        if (!event.getWorld().isRemote) {
           EntityCooldownCapability cap = event.getTarget().getCapability(AnimalCooldownCapabilityProvider.ANIMAL_COOLDOWN_CAPABILITY, null);
           if (cap != null) {
             if (cap.canHarvest()) {
@@ -41,11 +46,10 @@ public class SquidHandler {
               event.getWorld().playSound(player.posX, player.posY, player.posZ, SoundEvents.ENTITY_COW_MILK, SoundCategory.PLAYERS, 0.5F, event.getWorld().rand.nextFloat() * 0.25F + 0.6F, true);
               if (!player.isCreative()) heldItem.shrink(1);
               player.inventory.addItemStackToInventory(new ItemStack(ModItems.ink_bottle));
-              event.setCanceled(true);
-              event.setCancellationResult(EnumActionResult.SUCCESS);
+              return;
             }
           }
-
+          player.sendStatusMessage(new TextComponentTranslation("message.squid.cooldown").setStyle(new Style().setColor(TextFormatting.BLACK).setBold(true)), true);
         }
       }
     }
