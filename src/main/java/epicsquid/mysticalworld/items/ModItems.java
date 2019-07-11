eb99faf636fe453884a4faaf467192c408525e60
EpicSquid <epicsquid315@gmail.com>
2019-07-12 08:30:31 +1000

    Fixed squid and loot handlers

--- src/main/java/epicsquid/mysticalworld/items/ModItems.java
+++ src/main/java/epicsquid/mysticalworld/items/ModItems.java
@@ -1,10 +1,11 @@
 package epicsquid.mysticalworld.items;
 
 import epicsquid.mysticalworld.MysticalWorld;
+import net.minecraft.item.Item;
 import net.minecraftforge.registries.ObjectHolder;
 
 @ObjectHolder(MysticalWorld.MODID)
 public class ModItems {
 
-
+	public static final Item INK_BOTTLE = null;
 }
