3c6a351cdba5a88cd8a3a0c695b3ca94b1f8324d
EpicSquid <epicsquid315@gmail.com>
2019-07-11 17:04:03 +1000

    Just initial 1.14.3 things

--- src/main/java/epicsquid/mysticalworld/MysticalWorld.java
+++ src/main/java/epicsquid/mysticalworld/MysticalWorld.java
@@ -1,75 +1,29 @@
 package epicsquid.mysticalworld;
 
-import epicsquid.mysticallib.capability.EntityCooldownCapability;
-import epicsquid.mysticalworld.capability.AnimalCooldownCapability;
-import epicsquid.mysticalworld.capability.AnimalCooldownCapabilityStorage;
-import epicsquid.mysticalworld.init.ModItems;
-import epicsquid.mysticalworld.proxy.CommonProxy;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.fml.common.Loader;
+import epicsquid.mysticalworld.setup.ModSetup;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.ModContainer;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.*;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(modid = MysticalWorld.MODID, version = MysticalWorld.VERSION, name = MysticalWorld.NAME, dependencies = MysticalWorld.DEPENDENCIES)
+@Mod("mysticalworld")
 public class MysticalWorld {
-  public static final String MODID = "mysticalworld";
-  public static final String DOMAIN = "mysticalworld";
-  public static final String NAME = "Mystical World";
-  public static final String VERSION = "@VERSION@";
-  public static final String DEPENDENCIES = "required-before:mysticallib";
+	public static final String MODID = "mysticalworld";
 
-  public static ModContainer CONTAINER = null;
+//  public static CreativeTabs tab = new CreativeTabs("mysticalworld") {
+//    @Override
+//    public String getTabLabel() {
+//      return "mysticalworld";
+//    }
+//
+//    @Override
+//    @SideOnly(Side.CLIENT)
+//    public ItemStack createIcon() {
+//      return new ItemStack(ModItems.carapace, 1);
+//    }
+//  };
 
-  @SidedProxy(clientSide = "epicsquid.mysticalworld.proxy.ClientProxy", serverSide = "epicsquid.mysticalworld.proxy.CommonProxy")
-  public static CommonProxy proxy;
+	public static ModSetup setup = new ModSetup();
 
-  public static Logger logger;
-
-  @Instance(MODID) public static MysticalWorld instance;
-
-  public static CreativeTabs tab = new CreativeTabs("mysticalworld") {
-    @Override
-    public String getTabLabel() {
-      return "mysticalworld";
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public ItemStack createIcon() {
-      return new ItemStack(ModItems.carapace, 1);
-    }
-  };
-
-  @EventHandler
-  public void preInit(FMLPreInitializationEvent event) {
-    CONTAINER = Loader.instance().activeModContainer();
-    MinecraftForge.EVENT_BUS.register(new RegistryManager());
-    CapabilityManager.INSTANCE.register(AnimalCooldownCapability.class, new AnimalCooldownCapabilityStorage(), AnimalCooldownCapability::new);
-    logger = event.getModLog();
-    proxy.preInit(event);
-  }
-
-  public static MysticalWorld getInstance() {
-    return instance;
-  }
-
-  @EventHandler
-  public void init(FMLInitializationEvent event) {
-    proxy.init(event);
-  }
-
-  @EventHandler
-  public void postInit(FMLPostInitializationEvent event) {
-    proxy.postInit(event);
-  }
+	public MysticalWorld() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(setup::init);
+	}
 }
---
+++ src/main/java/epicsquid/mysticalworld/setup/ModSetup.java
@@ -0,0 +1,10 @@
+package epicsquid.mysticalworld.setup;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+public class ModSetup {
+
+	public void init(FMLCommonSetupEvent event) {
+
+	}
+}
