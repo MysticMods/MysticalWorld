f1f82458fa53baa45d8318e46b97b83305ab98e3
Jon McManus <due@wxwhatever.com>
2019-08-18 12:15:43 +1000

    Bump version for release.

--- src/main/java/epicsquid/mysticalworld/MysticalWorld.java
+++ src/main/java/epicsquid/mysticalworld/MysticalWorld.java
@@ -28,7 +28,7 @@ public class MysticalWorld {
   public static final String DOMAIN = "mysticalworld";
   public static final String NAME = "Mystical World";
   public static final String VERSION = "@VERSION@";
-  public static final String DEPENDENCIES = "required-before:mysticallib";
+  public static final String DEPENDENCIES = "required-before:mysticallib@1.3.3";
 
   public static ModContainer CONTAINER = null;
 
