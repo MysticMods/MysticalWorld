a8fa26ebedc4de5f4da0777f842c33141673db28
Jon McManus <due@wxwhatever.com>
2019-10-28 19:50:50 +1000

    Standard ingredient for Aubergine.

---
+++ src/main/java/epicsquid/mysticalworld/recipe/Ingredients.java
@@ -0,0 +1,7 @@
+package epicsquid.mysticalworld.recipe;
+
+import epicsquid.mysticallib.recipe.factories.MultiOreIngredient;
+
+public class Ingredients {
+  public static MultiOreIngredient AUBERGINE = new MultiOreIngredient("cropAubergine", "cropEggplant");
+}
