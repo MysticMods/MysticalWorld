7402d1981a0476c1be2889c2b56111a022a51986
Jon McManus <due@wxwhatever.com>
2019-10-30 16:03:14 +1000

    Convert all blocks over.

--- src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
+++ src/main/java/epicsquid/mysticalworld/init/ModBlocks.java
@@ -13,24 +13,24 @@ import net.minecraftforge.registries.ObjectHolder;
 
 @SuppressWarnings("unused")
 public class ModBlocks {
-  public static RegistryObject<ThatchBlock> THATCH = ModRegistries.registerBlock("thatch", ModRegistries.block(ThatchBlock::new, () -> Block.Properties.create(Material.WOOD).sound(SoundType.PLANT)), ModRegistries.SIG);
-  public static RegistryObject<AubergineCropBlock> AUBERGINE_CROP = ModRegistries.registerBlockWithoutItem("aubergine_crop", ModRegistries.block(AubergineCropBlock::new, () -> Block.Properties.create(Material.PLANTS).doesNotBlockMovement().hardnessAndResistance(0f).sound(SoundType.CROP)));
+  public static RegistryObject<ThatchBlock> THATCH = MysticalWorld.REGISTRY.registerBlock("thatch", MysticalWorld.REGISTRY.block(ThatchBlock::new, () -> Block.Properties.create(Material.WOOD).sound(SoundType.PLANT)), ModRegistries.SIG);
+  public static RegistryObject<AubergineCropBlock> AUBERGINE_CROP = MysticalWorld.REGISTRY.registerBlockWithoutItem("aubergine_crop", MysticalWorld.REGISTRY.block(AubergineCropBlock::new, () -> Block.Properties.create(Material.PLANTS).doesNotBlockMovement().hardnessAndResistance(0f).sound(SoundType.CROP).tickRandomly()));
 
   // Ore Blocks
-  public static RegistryObject<XPOreBlock> AMETHYST_ORE = ModRegistries.registerBlock(ModMaterials.AMETHYST.oreName(), ModRegistries.ore(XPOreBlock::new, ModMaterials.AMETHYST), ModRegistries.SMG);
-  public static RegistryObject<XPOreBlock> COPPER_ORE = ModRegistries.registerBlock(ModMaterials.COPPER.oreName(), ModRegistries.ore(XPOreBlock::new, ModMaterials.COPPER), ModRegistries.SMG);
-  public static RegistryObject<XPOreBlock> LEAD_ORE = ModRegistries.registerBlock(ModMaterials.LEAD.oreName(), ModRegistries.ore(XPOreBlock::new, ModMaterials.LEAD), ModRegistries.SMG);
-  public static RegistryObject<XPOreBlock> QUICKSILVER_ORE = ModRegistries.registerBlock(ModMaterials.QUICKSILVER.oreName(), ModRegistries.ore(XPOreBlock::new, ModMaterials.QUICKSILVER), ModRegistries.SMG);
-  public static RegistryObject<XPOreBlock> SILVER_ORE = ModRegistries.registerBlock(ModMaterials.SILVER.oreName(), ModRegistries.ore(XPOreBlock::new, ModMaterials.SILVER), ModRegistries.SMG);
-  public static RegistryObject<XPOreBlock> TIN_ORE = ModRegistries.registerBlock(ModMaterials.TIN.oreName(), ModRegistries.ore(XPOreBlock::new, ModMaterials.TIN), ModRegistries.SMG);
+  public static RegistryObject<BaseOreBlock> AMETHYST_ORE = MysticalWorld.REGISTRY.registerBlock(ModMaterials.AMETHYST.oreName(), MysticalWorld.REGISTRY.ore(BaseOreBlock::new, ModMaterials.AMETHYST), ModRegistries.SMG);
+  public static RegistryObject<BaseOreBlock> COPPER_ORE = MysticalWorld.REGISTRY.registerBlock(ModMaterials.COPPER.oreName(), MysticalWorld.REGISTRY.ore(BaseOreBlock::new, ModMaterials.COPPER), ModRegistries.SMG);
+  public static RegistryObject<BaseOreBlock> LEAD_ORE = MysticalWorld.REGISTRY.registerBlock(ModMaterials.LEAD.oreName(), MysticalWorld.REGISTRY.ore(BaseOreBlock::new, ModMaterials.LEAD), ModRegistries.SMG);
+  public static RegistryObject<BaseOreBlock> QUICKSILVER_ORE = MysticalWorld.REGISTRY.registerBlock(ModMaterials.QUICKSILVER.oreName(), MysticalWorld.REGISTRY.ore(BaseOreBlock::new, ModMaterials.QUICKSILVER), ModRegistries.SMG);
+  public static RegistryObject<BaseOreBlock> SILVER_ORE = MysticalWorld.REGISTRY.registerBlock(ModMaterials.SILVER.oreName(), MysticalWorld.REGISTRY.ore(BaseOreBlock::new, ModMaterials.SILVER), ModRegistries.SMG);
+  public static RegistryObject<BaseOreBlock> TIN_ORE = MysticalWorld.REGISTRY.registerBlock(ModMaterials.TIN.oreName(), MysticalWorld.REGISTRY.ore(BaseOreBlock::new, ModMaterials.TIN), ModRegistries.SMG);
 
   // Blocks
-  public static RegistryObject<Block> AMETHYST_BLOCK = ModRegistries.registerBlock(ModMaterials.AMETHYST.blockName(), ModRegistries.block(Block::new, ModMaterials.AMETHYST.getBlockProps()), ModRegistries.SMG);
-  public static RegistryObject<Block> COPPER_BLOCK = ModRegistries.registerBlock(ModMaterials.COPPER.blockName(), ModRegistries.block(Block::new, ModMaterials.COPPER.getBlockProps()), ModRegistries.SMG);
-  public static RegistryObject<Block> LEAD_BLOCK = ModRegistries.registerBlock(ModMaterials.LEAD.blockName(), ModRegistries.block(Block::new, ModMaterials.LEAD.getBlockProps()), ModRegistries.SMG);
-  public static RegistryObject<Block> QUICKSILVER_BLOCK = ModRegistries.registerBlock(ModMaterials.QUICKSILVER.blockName(), ModRegistries.block(Block::new, ModMaterials.QUICKSILVER.getBlockProps()), ModRegistries.SMG);
-  public static RegistryObject<Block> SILVER_BLOCK = ModRegistries.registerBlock(ModMaterials.SILVER.blockName(), ModRegistries.block(Block::new, ModMaterials.SILVER.getBlockProps()), ModRegistries.SMG);
-  public static RegistryObject<Block> TIN_BLOCK = ModRegistries.registerBlock(ModMaterials.TIN.blockName(), ModRegistries.block(Block::new, ModMaterials.TIN.getBlockProps()), ModRegistries.SMG);
+  public static RegistryObject<Block> AMETHYST_BLOCK = MysticalWorld.REGISTRY.registerBlock(ModMaterials.AMETHYST.blockName(), MysticalWorld.REGISTRY.block(Block::new, ModMaterials.AMETHYST.getBlockProps()), ModRegistries.SMG);
+  public static RegistryObject<Block> COPPER_BLOCK = MysticalWorld.REGISTRY.registerBlock(ModMaterials.COPPER.blockName(), MysticalWorld.REGISTRY.block(Block::new, ModMaterials.COPPER.getBlockProps()), ModRegistries.SMG);
+  public static RegistryObject<Block> LEAD_BLOCK = MysticalWorld.REGISTRY.registerBlock(ModMaterials.LEAD.blockName(), MysticalWorld.REGISTRY.block(Block::new, ModMaterials.LEAD.getBlockProps()), ModRegistries.SMG);
+  public static RegistryObject<Block> QUICKSILVER_BLOCK = MysticalWorld.REGISTRY.registerBlock(ModMaterials.QUICKSILVER.blockName(), MysticalWorld.REGISTRY.block(Block::new, ModMaterials.QUICKSILVER.getBlockProps()), ModRegistries.SMG);
+  public static RegistryObject<Block> SILVER_BLOCK = MysticalWorld.REGISTRY.registerBlock(ModMaterials.SILVER.blockName(), MysticalWorld.REGISTRY.block(Block::new, ModMaterials.SILVER.getBlockProps()), ModRegistries.SMG);
+  public static RegistryObject<Block> TIN_BLOCK = MysticalWorld.REGISTRY.registerBlock(ModMaterials.TIN.blockName(), MysticalWorld.REGISTRY.block(Block::new, ModMaterials.TIN.getBlockProps()), ModRegistries.SMG);
 
-  public static void init () {}
+  public static void load() {}
 }
