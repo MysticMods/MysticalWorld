e1122396ab1118fe1e9015236bfa12e62ae3dbd5
Jon McManus <due@wxwhatever.com>
2019-12-01 18:43:03 +1000

    A bunch of tweaks of things.

--- src/main/java/epicsquid/mysticalworld/data/MWRecipeProvider.java
+++ src/main/java/epicsquid/mysticalworld/data/MWRecipeProvider.java
@@ -64,6 +64,8 @@ public class MWRecipeProvider extends DeferredRecipeProvider {
     fenceGate(ModBlocks.TERRACOTTA_BRICK, ModBlocks.TERRACOTTA_BRICK_FENCE_GATE, "fence_gate", consumer);
     wall(ModBlocks.TERRACOTTA_BRICK, ModBlocks.TERRACOTTA_BRICK_WALL, false, consumer);
 
+    singleItemUnfinished(ModItems.UNRIPE_ENDER_PEARL, () -> Items.ENDER_PEARL, 9, 1).build(consumer, new ResourceLocation(MysticalWorld.MODID, "ender_pearl_from_unripe_pearls"));
+
     axe(ModItems.AMETHYST_GEM, ModItems.AMETHYST_AXE, "axe", consumer);
     axe(ModItems.COPPER_INGOT, ModItems.COPPER_AXE, "axe", consumer);
     axe(ModItems.SILVER_INGOT, ModItems.SILVER_AXE, "axe", consumer);
@@ -85,15 +87,12 @@ public class MWRecipeProvider extends DeferredRecipeProvider {
     storage(ModItems.TIN_NUGGET, ModItems.TIN_INGOT, consumer);
     storage(ModItems.LEAD_NUGGET, ModItems.LEAD_INGOT, consumer);
 
-    singleItemUnfinished(ModItems.UNRIPE_ENDER_PEARL, () -> Items.ENDER_PEARL, 9, 1).build(consumer, new ResourceLocation(MysticalWorld.MODID, "ender_pearl_from_unripe_pearls"));
-
     pickaxe(ModItems.AMETHYST_GEM, ModItems.AMETHYST_PICKAXE, "pickaxe", consumer);
     pickaxe(ModItems.COPPER_INGOT, ModItems.COPPER_PICKAXE, "pickaxe", consumer);
     pickaxe(ModItems.SILVER_INGOT, ModItems.SILVER_PICKAXE, "pickaxe", consumer);
     pickaxe(ModItems.QUICKSILVER_INGOT, ModItems.QUICKSILVER_PICKAXE, "pickaxe", consumer);
     pickaxe(ModItems.TIN_INGOT, ModItems.TIN_PICKAXE, "pickaxe", consumer);
     pickaxe(ModItems.LEAD_INGOT, ModItems.LEAD_PICKAXE, "pickaxe", consumer);
-    pickaxe(() -> Items.CACTUS, ModItems.CACTUS_PICKAXE, "pickaxe", consumer);
 
     sword(ModItems.AMETHYST_GEM, ModItems.AMETHYST_SWORD, "sword", consumer);
     sword(ModItems.COPPER_INGOT, ModItems.COPPER_SWORD, "sword", consumer);
@@ -101,7 +100,6 @@ public class MWRecipeProvider extends DeferredRecipeProvider {
     sword(ModItems.QUICKSILVER_INGOT, ModItems.QUICKSILVER_SWORD, "sword", consumer);
     sword(ModItems.TIN_INGOT, ModItems.TIN_SWORD, "sword", consumer);
     sword(ModItems.LEAD_INGOT, ModItems.LEAD_SWORD, "sword", consumer);
-    sword(() -> Items.CACTUS, ModItems.CACTUS_SWORD, "sword", consumer);
 
     shovel(ModItems.AMETHYST_GEM, ModItems.AMETHYST_SHOVEL, "shovel", consumer);
     shovel(ModItems.COPPER_INGOT, ModItems.COPPER_SHOVEL, "shovel", consumer);
@@ -109,7 +107,8 @@ public class MWRecipeProvider extends DeferredRecipeProvider {
     shovel(ModItems.QUICKSILVER_INGOT, ModItems.QUICKSILVER_SHOVEL, "shovel", consumer);
     shovel(ModItems.TIN_INGOT, ModItems.TIN_SHOVEL, "shovel", consumer);
     shovel(ModItems.LEAD_INGOT, ModItems.LEAD_SHOVEL, "shovel", consumer);
-    shovel(() -> Items.CACTUS, ModItems.CACTUS_SHOVEL, "shovel", consumer);
+
+
 
     hoe(ModItems.AMETHYST_GEM, ModItems.AMETHYST_HOE, "hoe", consumer);
     hoe(ModItems.COPPER_INGOT, ModItems.COPPER_HOE, "hoe", consumer);
@@ -117,7 +116,6 @@ public class MWRecipeProvider extends DeferredRecipeProvider {
     hoe(ModItems.QUICKSILVER_INGOT, ModItems.QUICKSILVER_HOE, "hoe", consumer);
     hoe(ModItems.TIN_INGOT, ModItems.TIN_HOE, "hoe", consumer);
     hoe(ModItems.LEAD_INGOT, ModItems.LEAD_HOE, "hoe", consumer);
-    hoe(() -> Items.CACTUS, ModItems.CACTUS_HOE, "hoe", consumer);
 
     knife(ModItems.AMETHYST_GEM, ModItems.AMETHYST_KNIFE, "knife", consumer);
     knife(ModItems.COPPER_INGOT, ModItems.COPPER_KNIFE, "knife", consumer);
@@ -125,13 +123,6 @@ public class MWRecipeProvider extends DeferredRecipeProvider {
     knife(ModItems.QUICKSILVER_INGOT, ModItems.QUICKSILVER_KNIFE, "knife", consumer);
     knife(ModItems.TIN_INGOT, ModItems.TIN_KNIFE, "knife", consumer);
     knife(ModItems.LEAD_INGOT, ModItems.LEAD_KNIFE, "knife", consumer);
-
-    knife(() -> Items.DIAMOND, ModItems.DIAMOND_KNIFE, "knife", consumer);
-    knife(() -> Items.GOLD_INGOT, ModItems.GOLD_KNIFE, "knife", consumer);
-    knife(() -> Items.IRON_INGOT, ModItems.IRON_KNIFE, "knife", consumer);
-    knife(ItemTags.PLANKS, ModItems.WOODEN_KNIFE, "knife", consumer);
-    knife(net.minecraftforge.common.Tags.Items.COBBLESTONE, ModItems.STONE_KNIFE, "knife", consumer);
-    knife(() -> Items.CACTUS, ModItems.CACTUS_KNIFE, "knife", consumer);
 
     helmet(ModItems.AMETHYST_GEM, ModItems.AMETHYST_HELMET, "helmet", consumer);
     helmet(ModItems.COPPER_INGOT, ModItems.COPPER_HELMET, "helmet", consumer);
@@ -161,6 +152,17 @@ public class MWRecipeProvider extends DeferredRecipeProvider {
     legs(ModItems.TIN_INGOT, ModItems.TIN_LEGGINGS, "leggings", consumer);
     legs(ModItems.LEAD_INGOT, ModItems.LEAD_LEGGINGS, "leggings", consumer);
 
+    pickaxe(() -> Items.CACTUS, ModItems.CACTUS_PICKAXE, "pickaxe", consumer);
+    sword(() -> Items.CACTUS, ModItems.CACTUS_SWORD, "sword", consumer);
+    shovel(() -> Items.CACTUS, ModItems.CACTUS_SHOVEL, "shovel", consumer);
+    hoe(() -> Items.CACTUS, ModItems.CACTUS_HOE, "hoe", consumer);
+    knife(() -> Items.DIAMOND, ModItems.DIAMOND_KNIFE, "knife", consumer);
+    knife(() -> Items.GOLD_INGOT, ModItems.GOLD_KNIFE, "knife", consumer);
+    knife(() -> Items.IRON_INGOT, ModItems.IRON_KNIFE, "knife", consumer);
+    knife(ItemTags.PLANKS, ModItems.WOODEN_KNIFE, "knife", consumer);
+    knife(net.minecraftforge.common.Tags.Items.COBBLESTONE, ModItems.STONE_KNIFE, "knife", consumer);
+    knife(() -> Items.CACTUS, ModItems.CACTUS_KNIFE, "knife", consumer);
+
     ore(MWTags.Items.AMETHYST_ORE, ModItems.AMETHYST_GEM, 0.3f, consumer);
     ore(MWTags.Items.COPPER_ORE, ModItems.COPPER_INGOT, 0.3f, consumer);
     ore(MWTags.Items.LEAD_ORE, ModItems.LEAD_INGOT, 0.3f, consumer);
@@ -180,8 +182,8 @@ public class MWRecipeProvider extends DeferredRecipeProvider {
     recycle(MWTags.Items.TIN_ITEMS, ModItems.TIN_NUGGET, 0.15f, consumer);
     recycle(MWTags.Items.LEAD_ITEMS, ModItems.LEAD_NUGGET, 0.15f, consumer);
 
-    recycle(ModItems.GOLD_KNIFE, () -> Items.GOLD_NUGGET, 0.15f, consumer);
-    recycle(ModItems.IRON_KNIFE, () -> Items.IRON_NUGGET, 0.15f, consumer);
+    recycle(ModItems.GOLD_KNIFE, () -> Items.GOLD_NUGGET, 0.15f, MysticalWorld.MODID, consumer);
+    recycle(ModItems.IRON_KNIFE, () -> Items.IRON_NUGGET, 0.15f, MysticalWorld.MODID, consumer);
 
     ShapelessRecipeBuilder.shapelessRecipe(ModItems.STUFFED_AUBERGINE.get(), 1).addIngredient(ModItems.COOKED_AUBERGINE.get()).addIngredient(MWTags.Items.VEGETABLES).addIngredient(MWTags.Items.VEGETABLES).addIngredient(MWTags.Items.COOKED_VEGETABLES).addCriterion("has_cooked_aubergine", this.hasItem(ModItems.COOKED_AUBERGINE.get())).build(consumer);
 
