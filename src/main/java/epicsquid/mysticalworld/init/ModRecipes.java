d5133c1a150a68532a646674c483dea3cd09ae01
Jon McManus <due@wxwhatever.com>
2020-02-08 15:29:16 +1000

    More efficient recipe for rockets.

--- src/main/java/epicsquid/mysticalworld/init/ModRecipes.java
+++ src/main/java/epicsquid/mysticalworld/init/ModRecipes.java
@@ -1,14 +1,8 @@
 package epicsquid.mysticalworld.init;
 
 import epicsquid.mysticalworld.MysticalWorld;
-import epicsquid.mysticalworld.recipe.FlintAndSteelRecipe;
-import epicsquid.mysticalworld.recipe.KnifeHornRecipe;
-import epicsquid.mysticalworld.recipe.KnifeRecipe;
-import epicsquid.mysticalworld.recipe.SpindleRecipe;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.item.crafting.IRecipeSerializer;
-import net.minecraft.item.crafting.IRecipeType;
-import net.minecraft.item.crafting.SpecialRecipeSerializer;
+import epicsquid.mysticalworld.recipe.*;
+import net.minecraft.item.crafting.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import net.minecraftforge.fml.RegistryObject;
@@ -18,6 +12,7 @@ public class ModRecipes {
   public static RegistryObject<IRecipeSerializer<FlintAndSteelRecipe>> FLINT_SERIALIZER = MysticalWorld.REGISTRY.registerRecipeSerializer("flint_and_steel_recipe", () -> new SpecialRecipeSerializer<>(FlintAndSteelRecipe::new));
   public static RegistryObject<IRecipeSerializer<SpindleRecipe>> SPINDLE_SERIALIZER = MysticalWorld.REGISTRY.registerRecipeSerializer("spindle_recipe", () -> new SpecialRecipeSerializer<>(SpindleRecipe::new));
   public static RegistryObject<IRecipeSerializer<KnifeHornRecipe>> HORN_SERIALIZER = MysticalWorld.REGISTRY.registerRecipeSerializer("knife_horn_recipe", () -> new SpecialRecipeSerializer<>(KnifeHornRecipe::new));
+  public static RegistryObject<IRecipeSerializer<BlazeRocketRecipe>> BLAZE_SERIALIZER = MysticalWorld.REGISTRY.registerRecipeSerializer("blaze_rocket_recipe", () -> new SpecialRecipeSerializer<>(BlazeRocketRecipe::new));
 
   private static <T extends IRecipe<?>> IRecipeType<T> register (String key) {
     return Registry.register(Registry.RECIPE_TYPE, new ResourceLocation(MysticalWorld.MODID, key), new IRecipeType<T>() {
---
+++ src/main/java/epicsquid/mysticalworld/recipe/BlazeRocketRecipe.java
@@ -0,0 +1,98 @@
+package epicsquid.mysticalworld.recipe;
+
+import epicsquid.mysticalworld.init.ModRecipes;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.SpecialRecipe;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+public class BlazeRocketRecipe extends SpecialRecipe {
+  private static final Ingredient INGREDIENT_PAPER = Ingredient.fromItems(Items.PAPER);
+  private static final Ingredient INGREDIENT_BLAZE_POWDER = Ingredient.fromItems(Items.BLAZE_POWDER);
+  private static final Ingredient INGREDIENT_FIREWORK_STAR = Ingredient.fromItems(Items.FIREWORK_STAR);
+
+  public BlazeRocketRecipe(ResourceLocation idIn) {
+    super(idIn);
+  }
+
+  public boolean matches(CraftingInventory inv, World worldIn) {
+    boolean flag = false;
+    int i = 0;
+
+    for (int j = 0; j < inv.getSizeInventory(); ++j) {
+      ItemStack itemstack = inv.getStackInSlot(j);
+      if (!itemstack.isEmpty()) {
+        if (INGREDIENT_PAPER.test(itemstack)) {
+          if (flag) {
+            return false;
+          }
+
+          flag = true;
+        } else if (INGREDIENT_BLAZE_POWDER.test(itemstack)) {
+          ++i;
+          if (i > 3) {
+            return false;
+          }
+        } else if (!INGREDIENT_FIREWORK_STAR.test(itemstack)) {
+          return false;
+        }
+      }
+    }
+
+    return flag && i >= 1;
+  }
+
+  public ItemStack getCraftingResult(CraftingInventory inv) {
+    ItemStack itemstack = new ItemStack(Items.FIREWORK_ROCKET, 5);
+    CompoundNBT compoundnbt = itemstack.getOrCreateChildTag("Fireworks");
+    ListNBT listnbt = new ListNBT();
+    int i = 0;
+
+    for (int j = 0; j < inv.getSizeInventory(); ++j) {
+      ItemStack itemstack1 = inv.getStackInSlot(j);
+      if (!itemstack1.isEmpty()) {
+        if (INGREDIENT_BLAZE_POWDER.test(itemstack1)) {
+          ++i;
+        } else if (INGREDIENT_FIREWORK_STAR.test(itemstack1)) {
+          CompoundNBT compoundnbt1 = itemstack1.getChildTag("Explosion");
+          if (compoundnbt1 != null) {
+            listnbt.add(compoundnbt1);
+          }
+        }
+      }
+    }
+
+    compoundnbt.putByte("Flight", (byte) (i * 2));
+    if (!listnbt.isEmpty()) {
+      compoundnbt.put("Explosions", listnbt);
+    }
+
+    return itemstack;
+  }
+
+  /**
+   * Used to determine if this recipe can fit in a grid of the given width/height
+   */
+  public boolean canFit(int width, int height) {
+    return width * height >= 2;
+  }
+
+  /**
+   * Get the result of this recipe, usually for display purposes (e.g. recipe book). If your recipe has more than one
+   * possible result (e.g. it's dynamic and depends on its inputs), then return an empty stack.
+   */
+  public ItemStack getRecipeOutput() {
+    return new ItemStack(Items.FIREWORK_ROCKET);
+  }
+
+  public IRecipeSerializer<?> getSerializer() {
+    return ModRecipes.BLAZE_SERIALIZER.get();
+  }
+}
+
