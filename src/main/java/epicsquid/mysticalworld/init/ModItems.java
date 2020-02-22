0d5c464f55e2671e741e0a3d303896f84718d3bd
Jon McManus <due@wxwhatever.com>
2020-02-22 10:43:21 +1000

    Move Assorted Seeds/Cooked Seeds into Mystical World.

--- src/main/java/epicsquid/mysticalworld/init/ModItems.java
+++ src/main/java/epicsquid/mysticalworld/init/ModItems.java
@@ -40,7 +40,7 @@ public class ModItems {
 
   public static Item silk_cocoon, silk_thread, spindle, silkworm_egg;
 
-  public static Item poisoned_potato;
+  public static Item poisoned_potato, seeds, cooked_seeds;
 
   /*  public static Item charm;*/
 
@@ -94,6 +94,14 @@ public class ModItems {
     }.setAlwaysEdible().setCreativeTab(MysticalWorld.tab));
     event.addItem(ink_bottle = new ItemBase("ink_bottle").setCreativeTab(MysticalWorld.tab).setContainerItem(Items.GLASS_BOTTLE));
     event.addItem(unripe_pearl = new ItemUnripePearl("unripe_pearl").setCreativeTab(MysticalWorld.tab));
+
+    event.addItem(seeds = new ItemBase("assorted_seeds").setCreativeTab(MysticalWorld.tab));
+    event.addItem(cooked_seeds = new ItemFoodBase("cooked_seeds", 1, 0.4f, false) {
+      @Override
+      public int getMaxItemUseDuration(ItemStack stack) {
+        return 8;
+      }
+    }.setCreativeTab(MysticalWorld.tab));
 
     if (!Loader.isModLoaded("roots") || !Knives.initKnives(event)) {
       event.addItem(amethyst_knife = new ItemKnifeBase("amethyst_knife", MaterialTypes.material("mysticalworld:amethyst")).setCreativeTab(MysticalWorld.tab));
