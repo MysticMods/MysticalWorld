dbba739c6c7874ef2e9c8283b117250d9c543e9f
Jon McManus <due@wxwhatever.com>
2020-03-06 22:37:40 +1000

    This isn't meant to be here. :/

--- src/main/java/epicsquid/mysticalworld/recipe/Ingredients.java
+++ src/main/java/epicsquid/mysticalworld/recipe/Ingredients.java
@@ -1,7 +1,12 @@
 package epicsquid.mysticalworld.recipe;
 
 import epicsquid.mysticallib.recipe.factories.MultiOreIngredient;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.oredict.OreIngredient;
 
 public class Ingredients {
   public static MultiOreIngredient AUBERGINE = new MultiOreIngredient("cropAubergine", "cropEggplant");
+  public static Ingredient BARK = new OreIngredient("rootsBark");
+  public static Ingredient RUNESTONE = new OreIngredient("runestone");
+  public static Ingredient FEY_LEATHER = new OreIngredient("feyLeather");
 }
