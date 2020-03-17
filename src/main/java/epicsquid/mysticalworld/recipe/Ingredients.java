d72d9a26d2f86c70885fa82dc4044cee7f006702
Jon McManus <due@wxwhatever.com>
2020-03-17 22:17:26 +1000

    Save some changes to materials.

--- src/main/java/epicsquid/mysticalworld/recipe/Ingredients.java
+++ src/main/java/epicsquid/mysticalworld/recipe/Ingredients.java
@@ -6,7 +6,4 @@ import net.minecraftforge.oredict.OreIngredient;
 
 public class Ingredients {
   public static MultiOreIngredient AUBERGINE = new MultiOreIngredient("cropAubergine", "cropEggplant");
-  public static Ingredient BARK = new OreIngredient("rootsBark");
-  public static Ingredient RUNESTONE = new OreIngredient("runestone");
-  public static Ingredient FEY_LEATHER = new OreIngredient("feyLeather");
 }
