f648c283c2f247567cba24eeda3137f951146e5a
Jon McManus <due@wxwhatever.com>
2020-04-07 20:27:45 +1000

    Addition compost-y things.

--- src/main/java/epicsquid/mysticalworld/RegistryManager.java
+++
@@ -1,7 +0,0 @@
-package epicsquid.mysticalworld;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod.EventBusSubscriber(modid = MysticalWorld.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class RegistryManager {
-}
--- src/main/java/epicsquid/mysticalworld/init/ModCompost.java
+++ src/main/java/epicsquid/mysticalworld/init/ModCompost.java
@@ -9,5 +9,8 @@ public class ModCompost {
     ComposterBlock.CHANCES.put(ModItems.AUBERGINE.get(), 0.65f);
     ComposterBlock.CHANCES.put(ModItems.STUFFED_AUBERGINE.get(), 1.5f);
     ComposterBlock.CHANCES.put(ModItems.AUBERGINE_SEEDS.get(), 0.3f);
+    ComposterBlock.CHANCES.put(ModItems.COOKED_BEETROOT.get(), 0.65f);
+    ComposterBlock.CHANCES.put(ModItems.COOKED_CARROT.get(), 0.65f);
+    ComposterBlock.CHANCES.put(ModItems.SLICED_CARROT.get(), 0.1625f);
   }
 }
