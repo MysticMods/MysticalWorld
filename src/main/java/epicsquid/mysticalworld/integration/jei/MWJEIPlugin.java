7531c67e95cf4e74f0c6a724f52d504b16ad8287
Jon McManus <due@wxwhatever.com>
2020-04-08 19:43:16 +1000

    Discard some of this.

--- src/main/java/epicsquid/mysticalworld/integration/jei/MWJEIPlugin.java
+++ src/main/java/epicsquid/mysticalworld/integration/jei/MWJEIPlugin.java
@@ -28,19 +28,9 @@ public class MWJEIPlugin implements IModPlugin {
 
   @Override
   public void registerVanillaCategoryExtensions(IVanillaCategoryExtensionRegistration registration) {
-/*    registration.getCraftingCategory().addCategoryExtension(KnifeRecipe.class, KnifeWrapper::new);
-    registration.getCraftingCategory().addCategoryExtension(SpindleRecipe.class, SpindleWrapper::new);
-    registration.getCraftingCategory().addCategoryExtension(FlintAndSteelRecipe.class, FlintAndSteelWrapper::new);
-    registration.getCraftingCategory().addCategoryExtension(KnifeHornRecipe.class, KnifeHornWrapper::new);*/
   }
 
   @Override
   public void registerRecipes(IRecipeRegistration registration) {
-    registration.addRecipes(Arrays.asList(
-/*        new KnifeRecipe(new ResourceLocation(MysticalWorld.MODID, "knife_temp")),
-        new SpindleRecipe(new ResourceLocation(MysticalWorld.MODID, "spindle_temp")),
-        new FlintAndSteelRecipe(new ResourceLocation(MysticalWorld.MODID, "flint_temp")),
-        new KnifeHornRecipe(new ResourceLocation(MysticalWorld.MODID, "horn_temp"))*/
-    ), new ResourceLocation("minecraft", "crafting"));
   }
 }
