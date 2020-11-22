bb02d266bdb032f05c10ee889c75b363f4b02459
Jon McManus <due@wxwhatever.com>
2020-11-22 16:58:51 +1000

    Properly register entity attributes.

--- src/main/java/epicsquid/mysticalworld/setup/CommonSetup.java
+++ src/main/java/epicsquid/mysticalworld/setup/CommonSetup.java
@@ -5,22 +5,20 @@ import epicsquid.mysticalworld.capability.AnimalCooldownCapability;
 import epicsquid.mysticalworld.capability.AnimalCooldownCapabilityStorage;
 import epicsquid.mysticalworld.capability.PlayerShoulderCapability;
 import epicsquid.mysticalworld.capability.PlayerShoulderCapabilityStorage;
+import epicsquid.mysticalworld.entity.*;
 import epicsquid.mysticalworld.events.CapabilityHandler;
 import epicsquid.mysticalworld.events.DamageHandler;
 import epicsquid.mysticalworld.events.EntityHandler;
 import epicsquid.mysticalworld.events.LootHandler;
 import epicsquid.mysticalworld.init.ModCompost;
 import epicsquid.mysticalworld.init.ModEntities;
-import epicsquid.mysticalworld.init.ModFeatures;
 import epicsquid.mysticalworld.network.Networking;
 import epicsquid.mysticalworld.potions.PotionRecipes;
-import epicsquid.mysticalworld.world.OreGen;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.DeferredWorkQueue;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
@@ -41,10 +39,19 @@ public class CommonSetup {
       CapabilityManager.INSTANCE.register(IPlayerShoulderCapability.class, new PlayerShoulderCapabilityStorage(), PlayerShoulderCapability::new);
 
       ShadedCommonSetup.init(event);
-/*      OreGen.registerOreGeneration();*/
+      /*      OreGen.registerOreGeneration();*/
       PotionRecipes.registerRecipes();
 
-
+      GlobalEntityTypeAttributes.put(ModEntities.BEETLE.get(), BeetleEntity.attributes().create());
+      GlobalEntityTypeAttributes.put(ModEntities.DEER.get(), DeerEntity.attributes().create());
+      GlobalEntityTypeAttributes.put(ModEntities.FROG.get(), FrogEntity.attributes().create());
+      GlobalEntityTypeAttributes.put(ModEntities.SILVER_FOX.get(), SilverFoxEntity.attributes().create());
+      GlobalEntityTypeAttributes.put(ModEntities.SPROUT.get(), SproutEntity.attributes().create());
+      GlobalEntityTypeAttributes.put(ModEntities.ENDERMINI.get(), EnderminiEntity.attributes().create());
+      GlobalEntityTypeAttributes.put(ModEntities.LAVA_CAT.get(), LavaCatEntity.attributes().create());
+      GlobalEntityTypeAttributes.put(ModEntities.OWL.get(), OwlEntity.attributes().create());
+      GlobalEntityTypeAttributes.put(ModEntities.SILKWORM.get(), SilkwormEntity.attributes().create());
+      GlobalEntityTypeAttributes.put(ModEntities.HELL_SPROUT.get(), HellSproutEntity.attributes().create());
     });
 
   }
@@ -53,7 +60,7 @@ public class CommonSetup {
   }
 
   public void serverAboutToStart(FMLServerAboutToStartEvent event) {
-/*    ModifyLoot.modify();*/
+    /*    ModifyLoot.modify();*/
   }
 
   @SuppressWarnings("Duplicates")
@@ -71,6 +78,6 @@ public class CommonSetup {
   }
 
   public void loadComplete(FMLLoadCompleteEvent event) {
-/*    ModFeatures.loadComplete();*/
+    /*    ModFeatures.loadComplete();*/
   }
 }
