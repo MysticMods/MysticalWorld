45c851274543454fe325b346d40cb3a7f3cf2e0e
Jon McManus <due@wxwhatever.com>
2020-12-10 19:10:38 +1000

    Nuke these.

--- src/main/java/epicsquid/mysticalworld/blocks/AdditionalSkullBlock.java
+++
@@ -1,9 +0,0 @@
-package epicsquid.mysticalworld.blocks;
-
-import net.minecraft.block.SkullBlock;
-
-public class AdditionalSkullBlock extends SkullBlock {
-  public AdditionalSkullBlock(ISkullType p_i48332_1_, Properties p_i48332_2_) {
-    super(p_i48332_1_, p_i48332_2_);
-  }
-}
