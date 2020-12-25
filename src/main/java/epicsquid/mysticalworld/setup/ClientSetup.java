f0ad4982375f5ff11b5cdee992b94f62a73cc109
Jon McManus <due@wxwhatever.com>
2020-12-25 11:12:56 +1000

    Remove these other petrified bushes.

--- src/main/java/epicsquid/mysticalworld/setup/ClientSetup.java
+++ src/main/java/epicsquid/mysticalworld/setup/ClientSetup.java
@@ -38,8 +38,6 @@ public class ClientSetup {
       RenderTypeLookup.setRenderLayer(ModBlocks.WILD_AUBERGINE_CROP.get(), rendertype);
       RenderTypeLookup.setRenderLayer(ModBlocks.THATCH.get(), rendertype);
       RenderTypeLookup.setRenderLayer(ModBlocks.ROYAL_STONEFLOWER.get(), rendertype);
-/*      RenderTypeLookup.setRenderLayer(ModBlocks.PETRIFIED_BUSH.get(), rendertype);
-      RenderTypeLookup.setRenderLayer(ModBlocks.PETRIFIED_GRASS.get(), rendertype);*/
 
       ShadedClientSetup.init(event);
 
