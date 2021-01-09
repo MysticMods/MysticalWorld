b84f93eab15c9deb0747a5156da3d7952b59216c
Jon McManus <due@wxwhatever.com>
2021-01-10 09:43:15 +1000

    Fix #220: use amethyst tag instead of amethyst item.

--- src/main/java/epicsquid/mysticalworld/potions/PotionRecipes.java
+++ src/main/java/epicsquid/mysticalworld/potions/PotionRecipes.java
@@ -1,11 +1,13 @@
 package epicsquid.mysticalworld.potions;
 
+import epicsquid.mysticalworld.MWTags;
 import epicsquid.mysticalworld.init.ModBlocks;
 import epicsquid.mysticalworld.init.ModEffects;
 import epicsquid.mysticalworld.init.ModItems;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.loot.LootTable;
 import net.minecraft.potion.PotionUtils;
 import net.minecraft.potion.Potions;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
@@ -13,7 +15,7 @@ import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 public class PotionRecipes {
   public static void registerRecipes() {
     BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.MUNDANE)), Ingredient.fromItems(ModItems.ANTLERS.get()), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.STRENGTH));
-    BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.AWKWARD)), Ingredient.fromItems(ModItems.AMETHYST_GEM.get()), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), ModEffects.SERENDIPITY_POTION.get()));
+    BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.AWKWARD)), Ingredient.fromTag(MWTags.Items.AMETHYST_GEM), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), ModEffects.SERENDIPITY_POTION.get()));
     BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.MUNDANE)), Ingredient.fromItems(ModBlocks.STONEPETAL.get()), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.LEAPING));
   }
 }
