2c45f52e6f962a9ff49658afb84ed476358c7729
Jon McManus <due@wxwhatever.com>
2021-01-24 09:16:07 +1000

    Store this (although not currently enabled).

---
+++ src/main/java/epicsquid/mysticalworld/blocks/BloodChestBlock.java
@@ -0,0 +1,215 @@
+/*package epicsquid.mysticalworld.blocks;
+
+import it.unimi.dsi.fastutil.floats.Float2FloatFunction;
+import net.minecraft.block.*;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.piglin.PiglinTasks;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.pathfinding.PathType;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.stats.Stat;
+import net.minecraft.stats.Stats;
+import net.minecraft.tileentity.ChestTileEntity;
+import net.minecraft.tileentity.IChestLid;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityMerger;
+import net.minecraft.util.*;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.List;
+
+@SuppressWarnings({"deprecation", "NullableProblems", "WeakerAccess", "unused"}) // TODO: remove unused
+public class BloodChestBlock extends Block implements IWaterLoggable {
+  public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+  public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+  protected static final VoxelShape SHAPE_SINGLE = Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 14.0D, 15.0D);
+
+  public BloodChestBlock(AbstractBlock.Properties builder) {
+    super(builder);
+    this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH).with(WATERLOGGED, false));
+  }
+
+  @Override
+  public BlockRenderType getRenderType(BlockState state) {
+    return BlockRenderType.ENTITYBLOCK_ANIMATED;
+  }
+
+  @Override
+  public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+    if (stateIn.get(WATERLOGGED)) {
+      worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+    }
+
+    return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+  }
+
+  @Override
+  public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+    return SHAPE_SINGLE;
+  }
+
+  @Override
+  public BlockState getStateForPlacement(BlockItemUseContext context) {
+    Direction direction = context.getPlacementHorizontalFacing().getOpposite();
+    FluidState fluidstate = context.getWorld().getFluidState(context.getPos());
+    return this.getDefaultState().with(FACING, direction).with(WATERLOGGED, fluidstate.getFluid() == Fluids.WATER);
+  }
+
+  @Override
+  public FluidState getFluidState(BlockState state) {
+    return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+  }
+
+  @Override
+  public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
+    if (stack.hasDisplayName()) {
+      TileEntity tileentity = worldIn.getTileEntity(pos);
+      if (tileentity instanceof ChestTileEntity) {
+        ((ChestTileEntity) tileentity).setCustomName(stack.getDisplayName());
+      }
+    }
+  }
+
+  @Override
+  public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+    if (!state.isIn(newState.getBlock())) {
+      TileEntity tileentity = worldIn.getTileEntity(pos);
+      if (tileentity instanceof IInventory) {
+        InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory) tileentity);
+        worldIn.updateComparatorOutputLevel(pos, this);
+      }
+
+      super.onReplaced(state, worldIn, pos, newState, isMoving);
+    }
+  }
+
+  @Override
+  public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+    if (worldIn.isRemote) {
+      return ActionResultType.SUCCESS;
+    } else {
+      INamedContainerProvider inamedcontainerprovider = this.getContainer(state, worldIn, pos);
+      if (inamedcontainerprovider != null) {
+        player.openContainer(inamedcontainerprovider);
+        player.addStat(this.getOpenStat());
+        PiglinTasks.func_234478_a_(player, true);
+      }
+
+      return ActionResultType.CONSUME;
+    }
+  }
+
+  protected Stat<ResourceLocation> getOpenStat() {
+    return Stats.CUSTOM.get(Stats.OPEN_CHEST);
+  }
+
+  @OnlyIn(Dist.CLIENT)
+  public static TileEntityMerger.ICallback<ChestTileEntity, Float2FloatFunction> getLidRotationCallback(final IChestLid lid) {
+    return new TileEntityMerger.ICallback<ChestTileEntity, Float2FloatFunction>() {
+      @Override
+      public Float2FloatFunction func_225539_a_(ChestTileEntity p_225539_1_, ChestTileEntity p_225539_2_) {
+        return (angle) -> Math.max(p_225539_1_.getLidAngle(angle), p_225539_2_.getLidAngle(angle));
+      }
+
+      @Override
+      public Float2FloatFunction func_225538_a_(ChestTileEntity p_225538_1_) {
+        return p_225538_1_::getLidAngle;
+      }
+
+      @Override
+      public Float2FloatFunction func_225537_b_() {
+        return lid::getLidAngle;
+      }
+    };
+  }
+
+  @Override
+  public TileEntity createTileEntity(BlockState state, IBlockReader worldIn) {
+    return new ChestTileEntity();
+  }
+
+  @Override
+  public boolean hasTileEntity(BlockState state) {
+    return true;
+  }
+
+  public static boolean isBlocked(IWorld world, BlockPos pos) {
+    return isBelowSolidBlock(world, pos) || isCatSittingOn(world, pos);
+  }
+
+  private static boolean isBelowSolidBlock(IBlockReader reader, BlockPos worldIn) {
+    BlockPos blockpos = worldIn.up();
+    return reader.getBlockState(blockpos).isNormalCube(reader, blockpos);
+  }
+
+  private static boolean isCatSittingOn(IWorld world, BlockPos pos) {
+    List<CatEntity> list = world.getEntitiesWithinAABB(CatEntity.class, new AxisAlignedBB((double) pos.getX(), (double) (pos.getY() + 1), (double) pos.getZ(), (double) (pos.getX() + 1), (double) (pos.getY() + 2), (double) (pos.getZ() + 1)));
+    if (!list.isEmpty()) {
+      for (CatEntity catentity : list) {
+        if (catentity.isEntitySleeping()) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public boolean hasComparatorInputOverride(BlockState state) {
+    return true;
+  }
+
+  @Override
+  public int getComparatorInputOverride(BlockState blockState, World worldIn, BlockPos pos) {
+    // TODO
+    return 0;
+  }
+
+  @Override
+  public BlockState rotate(BlockState state, Rotation rot) {
+    return state.with(FACING, rot.rotate(state.get(FACING)));
+  }
+
+  @Override
+  public BlockState mirror(BlockState state, Mirror mirrorIn) {
+    return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+  }
+
+  @Override
+  protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+    builder.add(FACING, WATERLOGGED);
+  }
+
+  @Override
+  public boolean allowsMovement(BlockState state, IBlockReader worldIn, BlockPos pos, PathType type) {
+    return false;
+  }
+
+  @Override
+  public boolean eventReceived(BlockState state, World worldIn, BlockPos pos, int id, int param) {
+    super.eventReceived(state, worldIn, pos, id, param);
+    TileEntity tileentity = worldIn.getTileEntity(pos);
+    return tileentity != null && tileentity.receiveClientEvent(id, param);
+  }
+}*/
---
+++ src/main/java/epicsquid/mysticalworld/tiles/BloodChestTileEntity.java
@@ -0,0 +1,467 @@
+/*package epicsquid.mysticalworld.tiles;
+
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.DoubleSidedInventory;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.inventory.container.ChestContainer;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
+import net.minecraft.loot.LootParameterSets;
+import net.minecraft.loot.LootParameters;
+import net.minecraft.loot.LootTable;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.state.properties.ChestType;
+import net.minecraft.tileentity.*;
+import net.minecraft.util.*;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.LockCode;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.Random;
+
+@SuppressWarnings({"ConstantConditions", "WeakerAccess"})
+@OnlyIn(
+    value = Dist.CLIENT,
+    _interface = IChestLid.class
+)
+public class BloodChestTileEntity extends TileEntity implements IChestLid, ITickableTileEntity {
+  private NonNullList<ItemStack> chestContents = NonNullList.withSize(27, ItemStack.EMPTY);
+  *//**
+   * The current angle of the lid (between 0 and 1)
+   *//*
+  protected float lidAngle;
+  *//**
+   * The angle of the lid last tick
+   *//*
+  protected float prevLidAngle;
+  *//**
+   * The number of players currently using this chest
+   *//*
+  protected int numPlayersUsing;
+  *//**
+   * A counter that is incremented once each tick. Used to determine when to recompute ; this is done every 200 ticks
+   * (but staggered between different chests). However, the new value isn't actually sent to clients when it is
+   * changed.
+   *//*
+  private int ticksSinceSync;
+  private net.minecraftforge.common.util.LazyOptional<net.minecraftforge.items.IItemHandlerModifiable> chestHandler;
+  private LockCode code = LockCode.EMPTY_CODE;
+  private ITextComponent customName;
+  protected ResourceLocation lootTable;
+  protected long lootTableSeed;
+
+  protected BloodChestTileEntity(TileEntityType<?> typeIn) {
+    super(typeIn);
+  }
+
+  public BloodChestTileEntity() {
+    this(TileEntityType.CHEST);
+  }
+
+  *//**
+   * Returns the number of slots in the inventory.
+   *//*
+  public int getSizeInventory() {
+    return 27;
+  }
+
+  protected ITextComponent getDefaultName() {
+    return new TranslationTextComponent("container.chest");
+  }
+
+  public void tick() {
+    int i = this.pos.getX();
+    int j = this.pos.getY();
+    int k = this.pos.getZ();
+    ++this.ticksSinceSync;
+    this.numPlayersUsing = calculatePlayersUsingSync(this.world, this, this.ticksSinceSync, i, j, k, this.numPlayersUsing);
+    this.prevLidAngle = this.lidAngle;
+    float f = 0.1F;
+    if (this.numPlayersUsing > 0 && this.lidAngle == 0.0F) {
+      this.playSound(SoundEvents.BLOCK_CHEST_OPEN);
+    }
+
+    if (this.numPlayersUsing == 0 && this.lidAngle > 0.0F || this.numPlayersUsing > 0 && this.lidAngle < 1.0F) {
+      float f1 = this.lidAngle;
+      if (this.numPlayersUsing > 0) {
+        this.lidAngle += 0.1F;
+      } else {
+        this.lidAngle -= 0.1F;
+      }
+
+      if (this.lidAngle > 1.0F) {
+        this.lidAngle = 1.0F;
+      }
+
+      float f2 = 0.5F;
+      if (this.lidAngle < 0.5F && f1 >= 0.5F) {
+        this.playSound(SoundEvents.BLOCK_CHEST_CLOSE);
+      }
+
+      if (this.lidAngle < 0.0F) {
+        this.lidAngle = 0.0F;
+      }
+    }
+
+  }
+
+  public static int calculatePlayersUsingSync(World p_213977_0_, BloodChestTileEntity p_213977_1_, int p_213977_2_, int p_213977_3_, int p_213977_4_, int p_213977_5_, int p_213977_6_) {
+    if (!p_213977_0_.isRemote && p_213977_6_ != 0 && (p_213977_2_ + p_213977_3_ + p_213977_4_ + p_213977_5_) % 200 == 0) {
+      p_213977_6_ = calculatePlayersUsing(p_213977_0_, p_213977_1_, p_213977_3_, p_213977_4_, p_213977_5_);
+    }
+
+    return p_213977_6_;
+  }
+
+  public static int calculatePlayersUsing(World p_213976_0_, BloodChestTileEntity p_213976_1_, int p_213976_2_, int p_213976_3_, int p_213976_4_) {
+    int i = 0;
+    float f = 5.0F;
+
+    for (PlayerEntity playerentity : p_213976_0_.getEntitiesWithinAABB(PlayerEntity.class, new AxisAlignedBB((double) ((float) p_213976_2_ - 5.0F), (double) ((float) p_213976_3_ - 5.0F), (double) ((float) p_213976_4_ - 5.0F), (double) ((float) (p_213976_2_ + 1) + 5.0F), (double) ((float) (p_213976_3_ + 1) + 5.0F), (double) ((float) (p_213976_4_ + 1) + 5.0F)))) {
+      if (playerentity.openContainer instanceof ChestContainer) {
+        IInventory iinventory = ((ChestContainer) playerentity.openContainer).getLowerChestInventory();
+        if (iinventory == p_213976_1_ || iinventory instanceof DoubleSidedInventory && ((DoubleSidedInventory) iinventory).isPartOfLargeChest(p_213976_1_)) {
+          ++i;
+        }
+      }
+    }
+
+    return i;
+  }
+
+  private void playSound(SoundEvent soundIn) {
+    ChestType chesttype = this.getBlockState().get(ChestBlock.TYPE);
+    if (chesttype != ChestType.LEFT) {
+      double d0 = (double) this.pos.getX() + 0.5D;
+      double d1 = (double) this.pos.getY() + 0.5D;
+      double d2 = (double) this.pos.getZ() + 0.5D;
+      if (chesttype == ChestType.RIGHT) {
+        Direction direction = ChestBlock.getDirectionToAttached(this.getBlockState());
+        d0 += (double) direction.getXOffset() * 0.5D;
+        d2 += (double) direction.getZOffset() * 0.5D;
+      }
+
+      this.world.playSound((PlayerEntity) null, d0, d1, d2, soundIn, SoundCategory.BLOCKS, 0.5F, this.world.rand.nextFloat() * 0.1F + 0.9F);
+    }
+  }
+
+  *//**
+   * See {@link Block#eventReceived} for more information. This must return true serverside before it is called
+   * clientside.
+   *//*
+  public boolean receiveClientEvent(int id, int type) {
+    if (id == 1) {
+      this.numPlayersUsing = type;
+      return true;
+    } else {
+      return super.receiveClientEvent(id, type);
+    }
+  }
+
+  public void openInventory(PlayerEntity player) {
+    if (!player.isSpectator()) {
+      if (this.numPlayersUsing < 0) {
+        this.numPlayersUsing = 0;
+      }
+
+      ++this.numPlayersUsing;
+      this.onOpenOrClose();
+    }
+
+  }
+
+  public void closeInventory(PlayerEntity player) {
+    if (!player.isSpectator()) {
+      --this.numPlayersUsing;
+      this.onOpenOrClose();
+    }
+
+  }
+
+  protected void onOpenOrClose() {
+    Block block = this.getBlockState().getBlock();
+    if (block instanceof ChestBlock) {
+      this.world.addBlockEvent(this.pos, block, 1, this.numPlayersUsing);
+      this.world.notifyNeighborsOfStateChange(this.pos, block);
+    }
+
+  }
+
+  protected NonNullList<ItemStack> getItems() {
+    return this.chestContents;
+  }
+
+  protected void setItems(NonNullList<ItemStack> itemsIn) {
+    this.chestContents = itemsIn;
+  }
+
+  @OnlyIn(Dist.CLIENT)
+  public float getLidAngle(float partialTicks) {
+    return MathHelper.lerp(partialTicks, this.prevLidAngle, this.lidAngle);
+  }
+
+  public static int getPlayersUsing(IBlockReader reader, BlockPos posIn) {
+    BlockState blockstate = reader.getBlockState(posIn);
+    if (blockstate.hasTileEntity()) {
+      TileEntity tileentity = reader.getTileEntity(posIn);
+      if (tileentity instanceof BloodChestTileEntity) {
+        return ((BloodChestTileEntity) tileentity).numPlayersUsing;
+      }
+    }
+
+    return 0;
+  }
+
+  public static void swapContents(BloodChestTileEntity chest, BloodChestTileEntity otherChest) {
+    NonNullList<ItemStack> nonnulllist = chest.getItems();
+    chest.setItems(otherChest.getItems());
+    otherChest.setItems(nonnulllist);
+  }
+
+  protected Container createMenu(int id, PlayerInventory player) {
+    return ChestContainer.createGeneric9X3(id, player, this);
+  }
+
+  @Override
+  public void updateContainingBlockInfo() {
+    super.updateContainingBlockInfo();
+    if (this.chestHandler != null) {
+      this.chestHandler.invalidate();
+      this.chestHandler = null;
+    }
+  }
+
+  @Override
+  public <T> net.minecraftforge.common.util.LazyOptional<T> getCapability(net.minecraftforge.common.capabilities.Capability<T> cap, Direction side) {
+    if (!this.removed && cap == net.minecraftforge.items.CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+      if (this.chestHandler == null)
+        this.chestHandler = net.minecraftforge.common.util.LazyOptional.of(this::createHandler);
+      return this.chestHandler.cast();
+    }
+    if (!this.removed && cap == net.minecraftforge.items.CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+      return itemHandler.cast();
+    return super.getCapability(cap, side);
+  }
+
+  private net.minecraftforge.items.IItemHandlerModifiable createHandler() {
+    BlockState state = this.getBlockState();
+    if (!(state.getBlock() instanceof ChestBlock)) {
+      return new net.minecraftforge.items.wrapper.InvWrapper(this);
+    }
+    IInventory inv = ChestBlock.getChestInventory((ChestBlock) state.getBlock(), state, getWorld(), getPos(), true);
+    return new net.minecraftforge.items.wrapper.InvWrapper(inv == null ? this : inv);
+  }
+
+  public static void setLootTable(IBlockReader reader, Random rand, BlockPos p_195479_2_, ResourceLocation lootTableIn) {
+    TileEntity tileentity = reader.getTileEntity(p_195479_2_);
+    if (tileentity instanceof LockableLootTileEntity) {
+      ((LockableLootTileEntity) tileentity).setLootTable(lootTableIn, rand.nextLong());
+    }
+
+  }
+
+  protected boolean checkLootAndRead(CompoundNBT compound) {
+    if (compound.contains("LootTable", 8)) {
+      this.lootTable = new ResourceLocation(compound.getString("LootTable"));
+      this.lootTableSeed = compound.getLong("LootTableSeed");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  protected boolean checkLootAndWrite(CompoundNBT compound) {
+    if (this.lootTable == null) {
+      return false;
+    } else {
+      compound.putString("LootTable", this.lootTable.toString());
+      if (this.lootTableSeed != 0L) {
+        compound.putLong("LootTableSeed", this.lootTableSeed);
+      }
+
+      return true;
+    }
+  }
+
+  public void fillWithLoot(@Nullable PlayerEntity player) {
+    if (this.lootTable != null && this.world.getServer() != null) {
+      LootTable loottable = this.world.getServer().getLootTableManager().getLootTableFromLocation(this.lootTable);
+      if (player instanceof ServerPlayerEntity) {
+        CriteriaTriggers.PLAYER_GENERATES_CONTAINER_LOOT.test((ServerPlayerEntity) player, this.lootTable);
+      }
+
+      this.lootTable = null;
+      LootContext.Builder lootcontext$builder = (new LootContext.Builder((ServerWorld) this.world)).withParameter(LootParameters.field_237457_g_, Vector3d.copyCentered(this.pos)).withSeed(this.lootTableSeed);
+      if (player != null) {
+        lootcontext$builder.withLuck(player.getLuck()).withParameter(LootParameters.THIS_ENTITY, player);
+      }
+
+      loottable.fillInventory(this, lootcontext$builder.build(LootParameterSets.CHEST));
+    }
+
+  }
+
+  public void setLootTable(ResourceLocation lootTableIn, long seedIn) {
+    this.lootTable = lootTableIn;
+    this.lootTableSeed = seedIn;
+  }
+
+  public boolean isEmpty() {
+    this.fillWithLoot((PlayerEntity) null);
+    return this.getItems().stream().allMatch(ItemStack::isEmpty);
+  }
+
+  *//**
+   * Returns the stack in the given slot.
+   *//*
+  public ItemStack getStackInSlot(int index) {
+    this.fillWithLoot((PlayerEntity) null);
+    return this.getItems().get(index);
+  }
+
+  *//**
+   * Removes up to a specified number of items from an inventory slot and returns them in a new stack.
+   *//*
+  public ItemStack decrStackSize(int index, int count) {
+    this.fillWithLoot((PlayerEntity) null);
+    ItemStack itemstack = ItemStackHelper.getAndSplit(this.getItems(), index, count);
+    if (!itemstack.isEmpty()) {
+      this.markDirty();
+    }
+
+    return itemstack;
+  }
+
+  *//**
+   * Removes a stack from the given slot and returns it.
+   *//*
+  public ItemStack removeStackFromSlot(int index) {
+    this.fillWithLoot((PlayerEntity) null);
+    return ItemStackHelper.getAndRemove(this.getItems(), index);
+  }
+
+  *//**
+   * Sets the given item stack to the specified slot in the inventory (can be crafting or armor sections).
+   *//*
+  public void setInventorySlotContents(int index, ItemStack stack) {
+    this.fillWithLoot((PlayerEntity) null);
+    this.getItems().set(index, stack);
+    if (stack.getCount() > this.getInventoryStackLimit()) {
+      stack.setCount(this.getInventoryStackLimit());
+    }
+
+    this.markDirty();
+  }
+
+  *//**
+   * Don't rename this method to canInteractWith due to conflicts with Container
+   *//*
+  public boolean isUsableByPlayer(PlayerEntity player) {
+    if (this.world.getTileEntity(this.pos) != this) {
+      return false;
+    } else {
+      return !(player.getDistanceSq((double) this.pos.getX() + 0.5D, (double) this.pos.getY() + 0.5D, (double) this.pos.getZ() + 0.5D) > 64.0D);
+    }
+  }
+
+  public void clear() {
+    this.getItems().clear();
+  }
+
+  public boolean canOpen(PlayerEntity p_213904_1_) {
+    return canUnlock(p_213904_1_, this.code, this.getDisplayName()) && (this.lootTable == null || !p_213904_1_.isSpectator());
+  }
+
+  @Nullable
+  public Container createMenu(int p_createMenu_1_, PlayerInventory p_createMenu_2_, PlayerEntity p_createMenu_3_) {
+    if (this.canOpen(p_createMenu_3_)) {
+      this.fillWithLoot(p_createMenu_2_.player);
+      return this.createMenu(p_createMenu_1_, p_createMenu_2_);
+    } else {
+      return null;
+    }
+  }
+
+  public void read(BlockState state, CompoundNBT nbt) {
+    super.read(state, nbt);
+    this.chestContents = NonNullList.withSize(this.getSizeInventory(), ItemStack.EMPTY);
+    if (!this.checkLootAndRead(nbt)) {
+      ItemStackHelper.loadAllItems(nbt, this.chestContents);
+    }
+    this.code = LockCode.read(nbt);
+    if (nbt.contains("CustomName", 8)) {
+      this.customName = ITextComponent.Serializer.getComponentFromJson(nbt.getString("CustomName"));
+    }
+  }
+
+  public CompoundNBT write(CompoundNBT compound) {
+    super.write(compound);
+    this.code.write(compound);
+    if (this.customName != null) {
+      compound.putString("CustomName", ITextComponent.Serializer.toJson(this.customName));
+    }
+    if (!this.checkLootAndWrite(compound)) {
+      ItemStackHelper.saveAllItems(compound, this.chestContents);
+    }
+
+
+    return compound;
+  }
+
+  public void setCustomName(ITextComponent name) {
+    this.customName = name;
+  }
+
+  public ITextComponent getName() {
+    return this.customName != null ? this.customName : this.getDefaultName();
+  }
+
+  public ITextComponent getDisplayName() {
+    return this.getName();
+  }
+
+  @Nullable
+  public ITextComponent getCustomName() {
+    return this.customName;
+  }
+
+  public static boolean canUnlock(PlayerEntity p_213905_0_, LockCode p_213905_1_, ITextComponent p_213905_2_) {
+    if (!p_213905_0_.isSpectator() && !p_213905_1_.func_219964_a(p_213905_0_.getHeldItemMainhand())) {
+      p_213905_0_.sendStatusMessage(new TranslationTextComponent("container.isLocked", p_213905_2_), true);
+      p_213905_0_.playSound(SoundEvents.BLOCK_CHEST_LOCKED, SoundCategory.BLOCKS, 1.0F, 1.0F);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  @Nullable
+  public Container createMenu(int p_createMenu_1_, PlayerInventory p_createMenu_2_, PlayerEntity p_createMenu_3_) {
+    return this.canOpen(p_createMenu_3_) ? this.createMenu(p_createMenu_1_, p_createMenu_2_) : null;
+  }
+
+  @Override
+  protected void invalidateCaps() {
+    super.invalidateCaps();
+    if (chestHandler != null)
+      chestHandler.invalidate();
+  }
+}*/
