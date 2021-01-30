cdbf4b407bc8386ec7c41e326f1730fca76cd51e
Jon McManus <due@wxwhatever.com>
2021-01-30 21:17:30 +1000

    Fix #229, properly make tags work.

--- src/main/java/epicsquid/mysticalworld/potions/PotionRecipes.java
+++ src/main/java/epicsquid/mysticalworld/potions/PotionRecipes.java
@@ -11,11 +11,13 @@ import net.minecraft.loot.LootTable;
 import net.minecraft.potion.PotionUtils;
 import net.minecraft.potion.Potions;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+import noobanidus.libs.noobutil.types.LazyIngredient;
 
 public class PotionRecipes {
   public static void registerRecipes() {
-    BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.MUNDANE)), Ingredient.fromItems(ModItems.ANTLERS.get()), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.STRENGTH));
-    BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.AWKWARD)), Ingredient.fromTag(MWTags.Items.AMETHYST_GEM), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), ModEffects.SERENDIPITY_POTION.get()));
-    BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.MUNDANE)), Ingredient.fromItems(ModBlocks.STONEPETAL.get()), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.LEAPING));
+
+    BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.MUNDANE)), new LazyIngredient(() -> Ingredient.fromItems(ModItems.ANTLERS.get())), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.STRENGTH));
+    BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.AWKWARD)), new LazyIngredient(() -> Ingredient.fromTag(MWTags.Items.AMETHYST_GEM)), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), ModEffects.SERENDIPITY_POTION.get()));
+    BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.MUNDANE)), new LazyIngredient(() -> Ingredient.fromItems(ModBlocks.STONEPETAL.get())), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.LEAPING));
   }
 }
