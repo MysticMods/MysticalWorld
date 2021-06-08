1998a7b951bb11f0f942849c9c80aab37d356c27
Jon McManus <due@wxwhatever.com>
2021-06-09 09:38:50 +1000

    Fix moved package in NoobUtil.

--- src/main/java/epicsquid/mysticalworld/world/SupplierBlockStateProvider.java
+++ src/main/java/epicsquid/mysticalworld/world/SupplierBlockStateProvider.java
@@ -4,7 +4,7 @@ import com.mojang.serialization.Codec;
 import epicsquid.mysticalworld.init.ModFeatures;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.gen.blockstateprovider.BlockStateProviderType;
-import noobanidus.libs.noobutil.types.AbstractSupplierBockStateProvider;
+import noobanidus.libs.noobutil.world.gen.provider.AbstractSupplierBockStateProvider;
 
 public class SupplierBlockStateProvider extends AbstractSupplierBockStateProvider {
   public static final Codec<SupplierBlockStateProvider> CODEC = codecBuilder(SupplierBlockStateProvider::new);
