682f49ac4a78eacc3077d12c919c6f2aae134ca6
Jon McManus <due@wxwhatever.com>
2021-07-04 10:28:41 +1000

    Work on #256: convert silkworm eggs into a non-spawn-egg item.
    It should no longer affect Vanilla spawners but does for reasons I'm
    unsure of.

--- src/main/java/mysticmods/mysticalworld/items/SilkwormEgg.java
+++ src/main/java/mysticmods/mysticalworld/items/SilkwormEgg.java
@@ -1,20 +1,109 @@
 package mysticmods.mysticalworld.items;
 
-import com.tterrag.registrate.util.LazySpawnEggItem;
 import mysticmods.mysticalworld.config.ConfigManager;
 import mysticmods.mysticalworld.entity.SilkwormEntity;
 import mysticmods.mysticalworld.init.ModEntities;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FlowingFluidBlock;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
+import net.minecraft.stats.Stats;
+import net.minecraft.tileentity.MobSpawnerTileEntity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.spawner.AbstractSpawner;
 
-public class SilkwormEgg extends LazySpawnEggItem<SilkwormEntity> {
+import java.util.Objects;
+
+public class SilkwormEgg extends Item {
   public SilkwormEgg(Properties builder) {
-    super(ModEntities.SILKWORM, 0, 0, builder);
+    super(builder);
+  }
+
+  private ActionResultType use(ItemUseContext context) {
+    World world = context.getWorld();
+    if (world.isRemote) {
+      return ActionResultType.SUCCESS;
+    } else {
+      ItemStack itemstack = context.getItem();
+      BlockPos blockpos = context.getPos();
+      Direction direction = context.getFace();
+      BlockState blockstate = world.getBlockState(blockpos);
+      Block block = blockstate.getBlock();
+      if (block == Blocks.SPAWNER) {
+        TileEntity tileentity = world.getTileEntity(blockpos);
+        if (tileentity instanceof MobSpawnerTileEntity) {
+          AbstractSpawner abstractspawner = ((MobSpawnerTileEntity) tileentity).getSpawnerBaseLogic();
+          EntityType<SilkwormEntity> entitytype1 = ModEntities.SILKWORM.get();
+          abstractspawner.setEntityType(entitytype1);
+          tileentity.markDirty();
+          world.notifyBlockUpdate(blockpos, blockstate, blockstate, 3);
+          itemstack.shrink(1);
+          return ActionResultType.SUCCESS;
+        }
+      }
+
+      BlockPos blockpos1;
+      if (blockstate.getCollisionShape(world, blockpos).isEmpty()) {
+        blockpos1 = blockpos;
+      } else {
+        blockpos1 = blockpos.offset(direction);
+      }
+
+      EntityType<SilkwormEntity> entitytype = ModEntities.SILKWORM.get();
+      if (entitytype.spawn((ServerWorld) world, itemstack, context.getPlayer(), blockpos1, SpawnReason.SPAWN_EGG, true, !Objects.equals(blockpos, blockpos1) && direction == Direction.UP) != null) {
+        itemstack.shrink(1);
+      }
+
+      return ActionResultType.SUCCESS;
+    }
+  }
+
+  private ActionResult<ItemStack> rightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+    ItemStack itemstack = playerIn.getHeldItem(handIn);
+    if (worldIn.isRemote) {
+      return new ActionResult<>(ActionResultType.PASS, itemstack);
+    } else {
+      RayTraceResult raytraceresult = rayTrace(worldIn, playerIn, RayTraceContext.FluidMode.SOURCE_ONLY);
+      if (raytraceresult.getType() != RayTraceResult.Type.BLOCK) {
+        return new ActionResult<>(ActionResultType.PASS, itemstack);
+      } else {
+        BlockRayTraceResult blockraytraceresult = (BlockRayTraceResult) raytraceresult;
+        BlockPos blockpos = blockraytraceresult.getPos();
+        if (!(worldIn.getBlockState(blockpos).getBlock() instanceof FlowingFluidBlock)) {
+          return new ActionResult<>(ActionResultType.PASS, itemstack);
+        } else if (worldIn.isBlockModifiable(playerIn, blockpos) && playerIn.canPlayerEdit(blockpos, blockraytraceresult.getFace(), itemstack)) {
+          EntityType<SilkwormEntity> entitytype = ModEntities.SILKWORM.get();
+          if (entitytype.spawn((ServerWorld) worldIn, itemstack, playerIn, blockpos, SpawnReason.SPAWN_EGG, false, false) == null) {
+            return new ActionResult<>(ActionResultType.PASS, itemstack);
+          } else {
+            if (!playerIn.abilities.isCreativeMode) {
+              itemstack.shrink(1);
+            }
+
+            playerIn.addStat(Stats.ITEM_USED.get(this));
+            return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
+          }
+        } else {
+          return new ActionResult<>(ActionResultType.FAIL, itemstack);
+        }
+      }
+    }
   }
 
   @Override
@@ -22,7 +111,7 @@ public class SilkwormEgg extends LazySpawnEggItem<SilkwormEntity> {
     World world = context.getWorld();
     if (!world.isRemote()) {
       if (world.getRandom().nextInt(ConfigManager.SILKWORM_CONFIG.getSuccessChance()) == 0) {
-        return super.onItemUse(context);
+        return use(context);
       } else {
         if (context.getPlayer() == null || !context.getPlayer().abilities.isCreativeMode) {
           context.getItem().shrink(1);
@@ -30,7 +119,7 @@ public class SilkwormEgg extends LazySpawnEggItem<SilkwormEntity> {
         return ActionResultType.SUCCESS;
       }
     } else {
-      return super.onItemUse(context);
+      return use(context);
     }
   }
 
@@ -39,7 +128,7 @@ public class SilkwormEgg extends LazySpawnEggItem<SilkwormEntity> {
     ItemStack itemstack = playerIn.getHeldItem(handIn);
     if (!worldIn.isRemote()) {
       if (worldIn.getRandom().nextInt(ConfigManager.SILKWORM_CONFIG.getSuccessChance()) == 0) {
-        return super.onItemRightClick(worldIn, playerIn, handIn);
+        return rightClick(worldIn, playerIn, handIn);
       } else {
         if (!playerIn.abilities.isCreativeMode) {
           itemstack.shrink(1);
@@ -47,7 +136,7 @@ public class SilkwormEgg extends LazySpawnEggItem<SilkwormEntity> {
         return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
       }
     } else {
-      return super.onItemRightClick(worldIn, playerIn, handIn);
+      return rightClick(worldIn, playerIn, handIn);
     }
   }
 }
