3fbaf9b097e7fe5d41f529ab0577a0b312049ce2
Jon McManus <due@wxwhatever.com>
2022-02-20 10:49:50 +1000

    Comment out Patchouli-related code.
    This needs to be re-synchronized back to Patchouli.

--- src/main/java/mysticmods/mysticalworld/items/GuideItem.java
+++ src/main/java/mysticmods/mysticalworld/items/GuideItem.java
@@ -1,13 +1,9 @@
 package mysticmods.mysticalworld.items;
 
 import mysticmods.mysticalworld.MysticalWorld;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -15,16 +11,12 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import vazkii.patchouli.common.base.PatchouliSounds;
 import vazkii.patchouli.common.book.Book;
 import vazkii.patchouli.common.book.BookRegistry;
-import vazkii.patchouli.common.network.NetworkHandler;
-import vazkii.patchouli.common.network.message.MessageOpenBookGui;
 
 import java.util.List;
 
+// TODO: Resynchronize with Patchouli
 @SuppressWarnings("NullableProblems")
 public class GuideItem extends Item {
   private static final ResourceLocation ENCYCLOPEDIA = new ResourceLocation(MysticalWorld.MODID, "world_guide");
@@ -44,13 +36,12 @@ public class GuideItem extends Item {
   }
 
   @Override
-  @OnlyIn(Dist.CLIENT)
   public void appendHoverText(ItemStack stack, Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
     super.appendHoverText(stack, worldIn, tooltip, flagIn);
     Book book = getBook();
-    if (book != null && book.contents != null) {
+/*    if (book != null && book.contents != null) {
       tooltip.add(book.getSubtitle().withStyle(ChatFormatting.GRAY));
-    }
+    }*/
   }
 
   @Override
@@ -60,11 +51,11 @@ public class GuideItem extends Item {
     if (book == null) {
       return InteractionResultHolder.fail(stack);
     } else {
-      if (playerIn instanceof ServerPlayer) {
+/*      if (playerIn instanceof ServerPlayer) {
         NetworkHandler.sendToPlayer(new MessageOpenBookGui(book.id, null), (ServerPlayer) playerIn);
         SoundEvent sfx = PatchouliSounds.getSound(book.openSound, PatchouliSounds.book_open);
         worldIn.playSound(null, playerIn.getX(), playerIn.getY(), playerIn.getZ(), sfx, SoundSource.PLAYERS, 1.0F, (float) (0.7D + Math.random() * 0.4D));
-      }
+      }*/
 
       return InteractionResultHolder.success(stack);
     }
