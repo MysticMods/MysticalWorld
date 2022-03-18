eb39b653aff52b70c61ae8eadd64b04495a0b1cd
Jon McManus <due@wxwhatever.com>
2022-03-18 10:08:37 +1000

    Fix a mushroom block to make it compile.

--- src/main/java/mysticmods/mysticalworld/blocks/UncannyMushroomBlock.java
+++ src/main/java/mysticmods/mysticalworld/blocks/UncannyMushroomBlock.java
@@ -1,6 +1,7 @@
 package mysticmods.mysticalworld.blocks;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -13,10 +14,9 @@ import java.util.function.Supplier;
 public class UncannyMushroomBlock extends AnywhereMushroomBlock {
   protected static final VoxelShape SHAPE = Block.box(3.0D, 0.0D, 3.0D, 13.0D, 11.0D, 13.0D);
 
-  public UncannyMushroomBlock(Properties props, Supplier<ConfiguredFeature<?, ?>> feature) {
-    super(props, feature);
+  public UncannyMushroomBlock(Properties p_153983_, Supplier<Holder<? extends ConfiguredFeature<?, ?>>> p_153984_) {
+    super(p_153983_, p_153984_);
   }
-
 
   @Override
   public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
